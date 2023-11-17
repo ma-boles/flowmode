a1c9e3e40a3855cc60b5035179d5ecbdb04349de
ma-boles <ma_boles@hotmail.com>
2023-11-17 14:31:28 -0800

    git commit -m "imported new component"

--- spotify-reads/src/App.js
+++ spotify-reads/src/App.js
@@ -1,7 +1,9 @@
+import Landing from './components/Landing';
 import Nav from './components/Nav';
 import BookShelf from './components/BookShelf';
 import Queue from './components/Queue';
 import WishList from './components/WishList';
+
 import './styles/App.css';
 
 function App() {
@@ -10,13 +12,7 @@ function App() {
         <Nav />
 
     <section className='app'>
-
-      <div className="header">
-        <h1>Keep track of <br></br>
-         your monthly <br></br>
-         Spotify audiobooks</h1>
-      </div>
-
+      <Landing />
       <BookShelf />
       <Queue />
       <WishList />
