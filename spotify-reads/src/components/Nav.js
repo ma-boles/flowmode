7eb20d876e70830fa1c05095df754e3c6de5979b
ma-boles <ma_boles@hotmail.com>
2023-11-17 16:36:37 -0800

    git commit -m "added time"

--- spotify-reads/src/components/Nav.js
+++ spotify-reads/src/components/Nav.js
@@ -11,6 +11,7 @@ export default function Nav() {
                     <a><li>Book Shelf</li></a>
                     <a><li>Queue</li></a>
                     <a><li>Wish List</li></a>
+                    <li className='time'>Time Left</li>
                 </ul>
             </section>
         </>
