bf8510e5c2b12ea8c303c339d8e585791b59159a
ma-boles <ma_boles@hotmail.com>
2023-11-17 16:35:33 -0800

    git commit -m "new component"

---
+++ spotify-reads/src/components/Book.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Book() {
+    return (
+        <>
+            <div className="book--container">
+                book img<br/>
+                book title<br/>
+                author<br/>
+            </div>
+        </>
+    )
+}
