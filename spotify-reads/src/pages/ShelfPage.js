cca4974d0244b6bdbc22cf9b960a921eaa3d49e7
ma-boles <ma_boles@hotmail.com>
2023-11-18 18:39:40 -0800

    git commit -m "updated layout"

--- spotify-reads/src/pages/ShelfPage.js
+++ spotify-reads/src/pages/ShelfPage.js
@@ -1,19 +1,32 @@
 import React from "react";
+import Nav from "../components/Nav";
 import BookShelf from "../components/BookShelf";
 import Queue from "../components/Queue";
-import WishList from "../components/WishList";
-import Footer from "../components/Footer";
+import ReadingList from "../components/ReadingList";
 
-import "../styles/App/css";
+import "../styles/App.css";
 import "../styles/Shelf.css"
 
 export default function ShelfPage() {
     return(
         <>
-        <BookShelf />
-        <Queue />
-        <WishList />
-        <Footer />
+        <Nav />
+        
+        <section className='bookshelf--section'>
+            <h2 className="bookshelf--h2">BookShelf</h2>
+            <BookShelf />
+        </section>
+
+        <section className='queue--section'>
+            <h3 className="queue--h3">Queue</h3>
+            <Queue />
+        </section>
+
+        <section className='readinglist--section'>
+            <h3 className="readinglist--h3">Reading List</h3>
+            <ReadingList />
+        </section>
+        
         </>
     )
 }
