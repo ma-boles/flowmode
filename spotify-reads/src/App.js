90818c65785569b75d624e7229d3932d94cb38cb
ma-boles <ma_boles@hotmail.com>
2023-11-18 18:37:51 -0800

    git commit -m "working on shelves"

--- spotify-reads/src/App.js
+++ spotify-reads/src/App.js
@@ -1,43 +1,26 @@
-import Nav from './components/Nav';
 import Landing from './components/Landing';
 import Works from './components/Works';
-import BookShelf from './components/BookShelf';
-import Queue from './components/Queue';
-import WishList from './components/WishList';
 import Footer from './components/Footer';
+import ShelfPage from './pages/ShelfPage';
 
+import "./pages/ShelfPage";
 import './styles/App.css';
 
 function App() {
   return (
     <>
-        <Nav />
 
     <div className='app'>
 
-      <section className='landing--section'>
+    <ShelfPage />
+      {/*<section className='landing--section'>
         <Landing />
       </section>
 
-      <Works />
-      {/*
-      <section className='bookshelf--section'>
-        <h2 className="bookshelf--h2">BookShelf</h2>
-        <BookShelf />
-      </section>
-
-      <section className='queue--section'>
-        <h3 className="queue--h3">Queue</h3>
-        <Queue />
-      </section>
-
-      <section className='wishlist--section'>
-        <h3 className="wishlist--h3">Wish List</h3>
-  <WishList />
-  </section>*/}
+      <Works />*/}
+      
 
       <Footer />
-
     </div>
     </>
 
