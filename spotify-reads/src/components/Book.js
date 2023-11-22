2a7a415e45e617fc8d2df919f79bee319bda4dcb
ma-boles <ma_boles@hotmail.com>
2023-11-21 18:18:55 -0800

    git commit -m "updated layout"

--- spotify-reads/src/components/Book.js
+++ spotify-reads/src/components/Book.js
@@ -1,5 +1,10 @@
 import React from "react";
-import ellipsis from "../images/ellipsis-solid.svg"
+import "../styles/Book.css"
+
+import ellipsis from "../images/ellipsis-solid.svg";
+import cover from "../images/image-solid.svg";
+import plus from "../images/plus-solid.svg";
+import remove from "../images/trash-solid.svg"
 
 export default function Book() {
 
@@ -15,34 +20,55 @@ export default function Book() {
     
     return (
         <>
-            <div className="book--container">
-                book img<br/>
-                book title<br/>
-                author<br/>
-                <img src={ellipsis} alt="ellipsis" className="book--ellipsis" onClick={handleEllipsisClick}></img>
+        <div className="book--container">
+
+            <div className="book--card">
+                <img src={cover} alt="book cover" className="book--cover"></img>
+
+                
+                <div className="book--info">
+                
+                    book title<br/>
+                    author<br/><br/>
+                    <strong><p className="book--p--time">10hr 25min</p></strong>
+
+                </div>
+
             </div>
+
+            <img src={ellipsis} alt="ellipsis" className="book--ellipsis" onClick={handleEllipsisClick}></img><br/>
 
             <div className="book--add" id="book--add">
 
                 <ul className="addlist">
                     <li className="addlist--add">
-                        <button className="addlist--button" onClick={add}>Add</button></li>
-
-                    <div className="addlist--add--options" id="addlist--add--options">
-                        <li className="addlist--li">
-                            <button className="addlist--button">Add to Bookshelf
-                                </button></li>
-                        <li className="addlist--li">
-                            <button className="addlist--button">Add to Queue</button></li>
-                        <li className="addlist--li">
-                            <button className="addlist--button">Add to Reading List</button></li>
-                    </div>
+                        <button className="addlist--button" onClick={add}>
+                            <img src={plus} className="book--icon"></img>
+                            Add</button>
+                    </li>
 
                     <li className="addlist--remove">
-                        <button className="addlist--button">Remove</button></li>
-                </ul>
+                        <button className="addlist--button">
+                            <img src={remove} className="book--icon"></img>Remove</button>
+                    </li>
 
+                </ul>
+            </div> 
+
+            <div className="addlist--add--options" id="addlist--add--options">
+                <li className="addlist--li">
+                    <button className="addlist--button">Add to Bookshelf</button>
+                </li>
+                <li className="addlist--li">
+                    <button className="addlist--button">Add to Queue</button>
+                </li>
+                <li className="addlist--li">
+                    <button className="addlist--button">Add to Reading List</button>
+                </li>
             </div>
+                
+        </div>
+            
         </>
     )
 }
