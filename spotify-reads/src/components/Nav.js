08f9c646dfae73513f0750448bf1646b139a7429
ma-boles <ma_boles@hotmail.com>
2023-11-22 17:07:32 -0800

    git commit -m "updated logo sizing"

--- spotify-reads/src/components/Nav.js
+++ spotify-reads/src/components/Nav.js
@@ -11,15 +11,18 @@ export default function Nav() {
     return(
         <>
             <section className='nav'>
-                <p className='logo--p'>Spotify Reads</p>
-
-                <ul>
-                    <a><li>Bookshelf</li></a>
-                    <a><li>Queue</li></a>
-                    <a><li>Reading List</li></a>
-                    <a><li>Catalogue</li></a>
-                    <li className='time' id='time' onClick={changeText}>Time Left</li>
-                </ul>
+                <div className='logo--section'>
+                    <p className='logo--p'>Spotify Reads</p>
+                </div>
+                <div className='links--section'>
+                    <ul>
+                        <a><li>Bookshelf</li></a>
+                        <a><li>Queue</li></a>
+                        <a><li>Reading List</li></a>
+                        <a><li>Catalogue</li></a>
+                        <li className='time' id='time' onClick={changeText}>Time Left</li>
+                    </ul>
+                </div>
             </section>
         </>
     )
