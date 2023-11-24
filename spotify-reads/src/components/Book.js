c1c063c1b8cacefaa4e186503a543aec6cf1d354
ma-boles <ma_boles@hotmail.com>
2023-11-23 17:24:33 -0800

    git commit -m "added props"

--- spotify-reads/src/components/Book.js
+++ spotify-reads/src/components/Book.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Book.css"
 
 import ellipsis from "../images/ellipsis-solid.svg";
@@ -6,17 +6,19 @@ import cover from "../images/image-solid.svg";
 import plus from "../images/plus-solid.svg";
 import remove from "../images/trash-solid.svg"
 
-export default function Book() {
+export default function Book({ title, author, book_time, onAddToBookshelf, onAddToQueue, onAddToReadingList, onRemove }) {
 
-    function handleEllipsisClick () {
-        var bookAdd = document.getElementById("book--add");
-        bookAdd.style.display = (bookAdd.style.display === 'none') ? 'block' : 'none'
+    const [ isEllipsisVisible, setEllipsisVisible ] = useState(false);
+    const [ isAddOptionsVisible, setAddOptionsVisible ] = useState(false);
+
+    const handleEllipsisClick = () => {
+        setEllipsisVisible(!isEllipsisVisible);
     };
 
-    function add (){
-        var addOptions = document.getElementById("addlist--add--options");
-        addOptions.style.display = (addOptions.style.display === 'none') ? 'block' : 'none'
-    }
+    const toggleAddOptions = () => {
+        setAddOptionsVisible(!isAddOptionsVisible);
+    };
+
     
     return (
         <>
@@ -25,50 +27,56 @@ export default function Book() {
             <div className="book--card">
                 <img src={cover} alt="book cover" className="book--cover"></img>
 
-                
                 <div className="book--info">
                 
-                    book title<br/>
-                    author<br/><br/>
-                    <strong><p className="book--p--time">10hr 25min</p></strong>
+                    {title}<br/>
+                    {author}<br/><br/>
+                    <strong><p className="book--p--time">{book_time}</p></strong>
 
                 </div>
-
             </div>
 
             <img src={ellipsis} alt="ellipsis" className="book--ellipsis" onClick={handleEllipsisClick}></img><br/>
 
-            <div className="book--add" id="book--add">
+            <div className={`book--add ${isEllipsisVisible ? "visible" : "" }`} id="book--add">
 
                 <ul className="addlist">
                     <li className="addlist--add">
-                        <button className="addlist--button" onClick={add}>
+                        <button className="addlist--button" onClick={toggleAddOptions}>
                             <img src={plus} className="book--icon"></img>
                             Add</button>
                     </li>
 
                     <li className="addlist--remove">
-                        <button className="addlist--button">
+                        <button className="addlist--button" onClick={onRemove}>
                             <img src={remove} className="book--icon"></img>Remove</button>
                     </li>
-
                 </ul>
             </div> 
 
-            <div className="addlist--add--options" id="addlist--add--options">
+            <div className={`addlist--add--options ${isAddOptionsVisible ? "visible": ""}`} id="addlist--add--options">
                 <li className="addlist--li">
-                    <button className="addlist--button">Add to Bookshelf</button>
+                    <button className="addlist--button" onClick={onAddToBookshelf}>Add to Bookshelf</button>
                 </li>
                 <li className="addlist--li">
-                    <button className="addlist--button">Add to Queue</button>
+                    <button className="addlist--button" onClick={onAddToQueue}>Add to Queue</button>
                 </li>
                 <li className="addlist--li">
-                    <button className="addlist--button">Add to Reading List</button>
+                    <button className="addlist--button" onClick={onAddToReadingList}>Add to Reading List</button>
                 </li>
             </div>
-                
         </div>
-            
         </>
     )
 }
+
+    /*
+    function handleEllipsisClick () {
+        var bookAdd = document.getElementById("book--add");
+        bookAdd.style.display = (bookAdd.style.display === 'none') ? 'block' : 'none'
+    };
+
+    function add (){
+        var addOptions = document.getElementById("addlist--add--options");
+        addOptions.style.display = (addOptions.style.display === 'none') ? 'block' : 'none'
+    }*/
