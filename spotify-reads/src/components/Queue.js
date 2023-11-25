624880b89256dd763f62a017a852b37703315ac5
ma-boles <ma_boles@hotmail.com>
2023-11-25 15:31:52 -0800

    git commit -m "imported BookSection"

--- spotify-reads/src/components/Queue.js
+++ spotify-reads/src/components/Queue.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Book from "./Book";
+import BookSection from "./BookSection";
 
 export default function Queue() {
+    const books = [
+        { id: 'book5', title:"Book 5", author:"Author 5", book_time:"7hr 45min" },
+        { id: 'book6', title:"Book 6", author:"Author 6", book_time:"7hr 45min" }
+    ]
+
     return (
         <>
             <div className="queue--container">
-                <Book title="Book 5" author="Author 5" book_time="7hr 45min"/>
-                <Book title="Book 6" author="Author 6" book_time="7hr 45min"/>
+                <BookSection books={books}/>
             </div>
         </>
     )
--- spotify-reads/src/components/ReadingList.js
+++ spotify-reads/src/components/ReadingList.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Book from "./Book";
+import BookSection from "./BookSection";
 import "../styles/Shelf.css"
 
 export default function ReadingList() {
+    const books = [
+        { id:'book3', title:"Book 3", author:"Author 3", book_time:"5hr 20min" },
+        { id:'book4', title:"Book 4", author:"Author 4", book_time:"5hr 20min" }
+    ]
     return (
         <>
             <div className="readinglist--container">
-                <Book title="Book 3" author="Author 3" book_time="5hr 20min"/>
-                <Book title="Book 4" author="Author 4" book_time="5hr 20min"/>
+                <BookSection books={books}/>
             </div>
         </>
     )
