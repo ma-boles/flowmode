944aaf4d155c56c545c22cff9cc888d5eada7e8a
ma-boles <ma_boles@hotmail.com>
2023-11-25 15:24:29 -0800

    gitcommit -m "imported BookSection"

--- spotify-reads/src/components/BookShelf.js
+++ spotify-reads/src/components/BookShelf.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Book from "./Book";
+import BookSection from "./BookSection";
 
 export default function BookShelf() {
+    const books = [
+        { id: 'book1', title:"Book 1", author:"Author 1", book_time:"10hr 25min" },
+        { id: 'book2', title:"Book 2", author:"Author 2", book_time:"10hr 25min" }
+    ]
     return (
         <>
             <div className="bookshelf--container">
-                <Book title="Book 1" author="Author 1" book_time="10hr 25min"/>
-                <Book title="Book 2" author="Author 2" book_time="10hr 25min"/>
-                
+                <BookSection sectionTitle="Bookshelf" books={books}/>
             </div>
         </>
     )
