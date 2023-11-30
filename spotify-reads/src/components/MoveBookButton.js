4b82095ae9068fdab22c70d1bf143f15e3c9c1b7
ma-boles <ma_boles@hotmail.com>
2023-11-29 17:55:34 -0800

    git commit -m "add/remove icons added"

--- spotify-reads/src/components/MoveBookButton.js
+++ spotify-reads/src/components/MoveBookButton.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/Book.css"
+import plus from "../images/plus-solid.svg";
+import remove from "../images/trash-solid.svg"
 
 export default function MoveBookButton({ book, targetShelf, actionType, onMoveBook, onRemove }) {
 
@@ -14,6 +16,7 @@ export default function MoveBookButton({ book, targetShelf, actionType, onMoveBo
     return(
         <>
             <button onClick={handleButtonClick} className="move--button">
+                {actionType === 'move' ? <img src={plus} alt="add" className="book--icon"></img> : <img src={remove} alt="remove" className="book--icon"></img> }
                 {actionType === 'move' ? `Add to ${targetShelf}`: 'Remove'}
             </button>
         </>
