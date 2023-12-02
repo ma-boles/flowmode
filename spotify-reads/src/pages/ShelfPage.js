0ac264b433da540afd29fdd6aea17dd54e5aeb62
ma-boles <ma_boles@hotmail.com>
2023-12-01 18:19:30 -0800

    git commit -m "updated MoveButton prop"

--- spotify-reads/src/pages/ShelfPage.js
+++ spotify-reads/src/pages/ShelfPage.js
@@ -12,6 +12,80 @@ import "../styles/Shelf.css";
 
 export default function ShelfPage() {
 
+
+
+// book data
+const [ shelves, setShelves ] = useState({
+
+    bookshelf: [
+        { id: 1, title:"Book 1", author:"Author 1", book_time: "10hr 25min"},
+        { id: 2, title:"Book 2", author:"Author 2", book_time:"10hr 25min" }
+    ],
+    queue: [
+        { id: 5, title:"Book 5", author:"Author 5", book_time:"7hr 45min" },
+        { id: 6, title:"Book 6", author:"Author 6", book_time:"7hr 45min" }
+    ],
+    readingList: [
+        { id: 3, title:"Book 3", author:"Author 3", book_time:"5hr 20min" },
+        { id: 4, title:"Book 4", author:"Author 4", book_time:"5hr 20min" }
+        ],
+});
+
+
+    
+// book add/remove logic
+const handleMoveBook = (book, targetShelfIndex) => {
+    //find index of book in current shelf
+    const currentShelfIndex = shelves.findIndex((shelf) => shelf.some((b) => b.id === book.id));
+    //create copies of current and target shelves' book arrays
+    const currentShelfBooks = [...shelves[currentShelfIndex]];
+    const targetShelfBooks = [...shelves[targetShelfIndex]];
+    
+    // remove book from current shelf
+    const currentIndex = currentShelfBooks.findIndex((b) => b.id === book.id);
+    currentShelfBooks.splice(currentIndex, 1);
+    // add book to target shelf
+    targetShelfBooks.push(book);
+
+    // update state with modified shelves
+    setShelves((prevShelves) => {
+        const updatedShelves = [...prevShelves];
+        updatedShelves[currentShelfIndex] = currentShelfBooks;
+        updatedShelves[targetShelfIndex] = targetShelfBooks;
+        return updatedShelves;
+    })
+};
+
+const handleRemoveBook = (book, shelfName) => {
+    setShelves((prevShelves) => {
+console.log('prevShelves:', prevShelves);
+console.log('shelfName:', shelfName);
+
+const currentShelf = prevShelves[shelfName];
+console.log('currentShelf:', currentShelf);
+
+    // find index of book in current shelf
+    const currentShelfIndex = currentShelf.findIndex((b) => b.id === book.id);
+
+    if(currentShelfIndex === -1) {
+        //book not found in and shelf
+        return prevShelves;
+    }
+
+    // create copy of current shelf books array
+    const currentShelfBooks = [...prevShelves[shelfName]];
+    currentShelfBooks.splice(currentShelfIndex, 1);
+
+    // create a copy of the shelves array
+    const updatedShelves = {
+        ...prevShelves,
+        [shelfName]: currentShelfBooks,
+    };
+
+        return updatedShelves;
+    });
+};
+
 //function to calculate total time for shelf
 const calculateTotalTime = (books) => {
     let totalHours = 0;
@@ -28,8 +102,6 @@ const calculateTotalTime = (books) => {
         const minutesMatch = minutesStr.match(/\d+/); //extract digits from string
         const minutes = parseInt(minutesMatch, 10) || 0;
         totalMinutes += minutes;
-
-        console.log(`${book.title}: ${hours} hr ${minutes} mins`);
         
     }
 
@@ -165,75 +237,4 @@ useEffect(() => {
 
 
 
-{/*
-// book data
-const [ shelves, setShelves ] = useState({
 
-    bookshelf: [
-        { id: 1, title:"Book 1", author:"Author 1", book_time: "10hr 25min"},
-        { id: 2, title:"Book 2", author:"Author 2", book_time:"10hr 25min" }
-    ],
-    queue: [
-        { id: 5, title:"Book 5", author:"Author 5", book_time:"7hr 45min" },
-        { id: 6, title:"Book 6", author:"Author 6", book_time:"7hr 45min" }
-    ],
-    readingList: [
-        { id: 3, title:"Book 3", author:"Author 3", book_time:"5hr 20min" },
-        { id: 4, title:"Book 4", author:"Author 4", book_time:"5hr 20min" }
-        ],
-});
-
-*/}
-
-{/*
-// book add/remove logic
-const handleMoveBook = (book, targetShelfIndex) => {
-    //find index of book in current shelf
-    const currentShelfIndex = shelves.findIndex((shelf) => shelf.some((b) => b.id === book.id));
-    //create copies of current and target shelves' book arrays
-    const currentShelfBooks = [...shelves[currentShelfIndex]];
-    const targetShelfBooks = [...shelves[targetShelfIndex]];
-    
-    // remove book from current shelf
-    const currentIndex = currentShelfBooks.findIndex((b) => b.id === book.id);
-    currentShelfBooks.splice(currentIndex, 1);
-    // add book to target shelf
-    targetShelfBooks.push(book);
-
-    // update state with modified shelves
-    setShelves((prevShelves) => {
-        const updatedShelves = [...prevShelves];
-        updatedShelves[currentShelfIndex] = currentShelfBooks;
-        updatedShelves[targetShelfIndex] = targetShelfBooks;
-        return updatedShelves;
-    })
-};
-
-const handleRemoveBook = (book) => {
-    setShelves((prevShelves) => {
-
-    // find index of book in current shelf
-    const currentShelfIndex = prevShelves.findIndex((shelf) => shelf.some((b) => b.id === book.id));
-
-    if(currentShelfIndex === -1) {
-        //book not found in and shelf
-        return prevShelves;
-    }
-
-    // create copy of current shelf books array
-    const currentShelfBooks = [...prevShelves[currentShelfIndex]];
-
-    // remove book from its current shelf
-    const currentIndex = currentShelfBooks.findIndex((b) => b.id === book.id);
-    currentShelfBooks.splice(currentIndex, 1);
-    
-    // create a copy of the shelves array
-    const updatedShelves = [...prevShelves];
-
-    // update state with modified shelf
-        updatedShelves[currentShelfIndex] = currentShelfBooks;
-
-        return updatedShelves;
-    });
-};
-*/}
