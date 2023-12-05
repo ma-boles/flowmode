22a1ff8e05e8716cf9e93e59e22ddd426eb3f2ca
ma-boles <ma_boles@hotmail.com>
2023-12-04 16:58:53 -0800

    git commit -m "imported ShelfProvider"

--- spotify-reads/src/index.js
+++ spotify-reads/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ShelfProvider } from './components/ShelfContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShelfProvider>
+      <App />
+    </ShelfProvider>
   </React.StrictMode>
 );
 
