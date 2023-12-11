aeefcbbb5e2a3b49ec85845c665989d4313d96fe
ma-boles <ma_boles@hotmail.com>
2023-12-11 14:10:46 -0800

    git commit -m "tailwindcss imported"

--- spotify-reads/src/index.js
+++ spotify-reads/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/tailwindcss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ShelfProvider } from './components/ShelfContext';
