b7d9ee839802b48bfee07280bc08efb222f0bce9
ma-boles <ma_boles@hotmail.com>
2023-12-12 18:11:50 -0800

    git commit -m "updated content"

--- spotify-reads/tailwind.config.js
+++ spotify-reads/tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: ['./src/**/*.js'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
