a4a4c04f5a976f1dd53677793a50ce9f2f42b9b2
ma-boles <ma_boles@hotmail.com>
2023-12-13 15:50:19 -0800

    git commit -m "updated tailwind styles"

--- spotify-reads/src/components/Nav.js
+++ spotify-reads/src/components/Nav.js
@@ -9,19 +9,19 @@ export default function Nav({ scrollToRef }) {
 
     return(
         <>
-            <section className='nav flex items-center w-full my-0 mx-4'>
+            <section className='flex justify-between my-0 mx-4 w-full'>
                 <div className='mt-4 ml-4'>
-                    <p className='pt-20 font-bold text-lg mt-8 ml-8'>Spotify Reads</p>
+                    <p className='mt-2 ml-4 font-bold text-base'>Spotify Reads</p>
                 </div>
                 <div className='links--section'>
-                    <ul className='list-none mr-8'>
+                    <ul className='mr-8 list-none'>
                         <li onClick={() => scrollToRef('Bookshelf')}>Bookshelf</li>
                         <li onClick={() => scrollToRef('Queue')}>Queue</li>
                         <li onClick={() => scrollToRef('ReadingList')}>Reading List</li>
 
                         <li>Catalogue</li>
 
-                        <li className='bg-transparent p-1 border-1 border-double border-whitesmoke text-base w-20 text-center cursor-pointer' id='time' onClick={changeText}>Time Left</li>
+                        <li className='p-1 text-center text-base bg-transparent w-20 border-solid border-2 border-neutral-100 cursor-pointer' id='time' onClick={changeText}>Time Left</li>
                     </ul>
                 </div>
             </section>
