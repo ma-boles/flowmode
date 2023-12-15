e973c8bc6b06008103e6631b40aebc832b66192a
ma-boles <ma_boles@hotmail.com>
2023-12-15 15:50:09 -0800

    git commit -m "updated tailwind styles"

--- spotify-reads/src/components/Landing.js
+++ spotify-reads/src/components/Landing.js
@@ -4,10 +4,10 @@ import Logo from "../images/Landing1.jpg"
 export default function Landing() {
     return (
         <>
-        <h2 className="text-right text-black font-bold">Spotify Reads</h2>
+        <h2 className="text-right text-lg text-black font-bold">Spotify Reads</h2>
 
         <section className="mx-auto">
-            <img src={Logo} alt="astronaut reading a book" />
+            <img src={Logo} alt="astronaut reading a book" className="mx-auto h-auto"/>
         </section>
         </>
     )
