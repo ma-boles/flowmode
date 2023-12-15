ce464b67f111412cdb8128783c9b0b11577bc85c
ma-boles <ma_boles@hotmail.com>
2023-12-15 15:51:59 -0800

    git commit -m "updated tailwind styles"

--- spotify-reads/src/components/Works.js
+++ spotify-reads/src/components/Works.js
@@ -8,44 +8,48 @@ export default function Works() {
 
             <h1 className="mb-6 pb-4">How it works</h1>
 
-                <div className="inline-flex">
-                    <div className="works--card">
-                        <div className="works--number">
+                <div className="inline-flex flex-wrap">
+                    <div className="m-8 p-8 w-64 h-68 rounded-lg works--card">
+                        <div className="mx-auto mb-3 w-12 border-2 border-single border-white">
                             1<br/>
                         </div>
-                        <h3 className="works--h3">Explore Spotify Catalogue:</h3>
+                        <h3 className="mb-2 font-bold">Explore Spotify Catalogue:</h3>
                         <p> Browse the Spotify catalogue to discover a variety of books.</p>
                     </div>
 
-                    <div className="works--card">
-                        <div className="works--number">
+                    <div className="m-8 p-8 w-64 h-68 rounded-lg works--card">
+                        <div className="mx-auto mb-3 w-12 border-2 border-single border-white">
                             2<br/>
                         </div>
-                        <h3 className="works--h3">Build your Reading List:</h3>
+                        <h3 className="mb-2 font-bold">Build your Reading List:</h3>
                         <p> Add books to your reading list for easy access.</p>
                     </div>
 
-                    <div className="works--card">
-                        <div className="works--number">
+                    <div className="m-8 p-8 w-64 h-68 rounded-lg works--card">
+                        <div className="mx-auto mb-3 w-12 border-2 border-single border-white">
                             3<br/>
                         </div>
-                        <h3 className="works--h3">Organize Your Selection:</h3>
+                        <h3 className="mb-2 font-bold">Organize Your Selection:</h3>
                         <p>Choose books from your reading list to either add to the queue or directly place them on your bookshelf.</p>
                     </div>
 
-                    <div className="works--card">
-                        <div className="works--number">
+                    <div className="m-8 p-8 w-64 h-68 rounded-lg works--card">
+                        <div className="mx-auto mb-3 w-12 border-2 border-single border-white">
                             4<br/>
                         </div>
-                        <h3 className="works--h3">Listen and Enjoy!</h3>
+                        <h3 className="mb-2 font-bold">Listen and Enjoy!</h3>
                         <p>Sit back, relax, and enjoy listening to selected books on Spotify.</p>
                     </div>
                 </div>
-                <a><button className="py-2 px-14 mt-10 mb-6 cursor-pointer bg-transparent rounded-full border-1 border-double border-white">
+                <div>
+                <a><button className="py-2 px-20 mt-10 mb-6 bg-transparent border-2 border-double border-white rounded-full">
                     Log In
                 </button>
                 </a>
+                </div>
+                
             </section>
+            
         </>
     )
 }
