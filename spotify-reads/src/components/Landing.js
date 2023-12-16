b52dec43f41fcdf0cc4cc8757b68bab04e25a800
ma-boles <ma_boles@hotmail.com>
2023-12-16 14:50:20 -0800

    git commit -m "added new image"

--- spotify-reads/src/components/Landing.js
+++ spotify-reads/src/components/Landing.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Logo from "../images/Landing1.jpg"
+import Logo from "../images/LandingImg5.jpg"
 
 export default function Landing() {
     return (
         <>
         <h2 className="text-right text-lg text-black font-bold">Spotify Reads</h2>
 
-        <section className="mx-auto">
-            <img src={Logo} alt="astronaut reading a book" className="mx-auto h-auto"/>
+        <section className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center">
+            <img src={Logo} alt="astronaut reading a book" className="w-full h-auto"/>
         </section>
         </>
     )
