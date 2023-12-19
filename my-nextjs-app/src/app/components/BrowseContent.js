879f000970672417189cf52e4a3429417245333f
ma-boles <ma_boles@hotmail.com>
2023-12-18 19:31:06 -0800

    git commit -m "new content files"

---
+++ my-nextjs-app/src/app/components/BrowseContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/styles.css";
+
+
+export default function Browse() {
+    return(
+        <>
+            <section className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center">
+                <img src="/Browse2.jpg" alt="stack of books" className="w-full h-auto"></img>
+            </section>
+        </>
+    )
+}
