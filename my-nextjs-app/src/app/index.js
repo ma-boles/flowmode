1167a7de8cd5a5817e05bd43d502c2ba4cfcd819
ma-boles <ma_boles@hotmail.com>
2023-12-18 19:28:16 -0800

    git commit -m "removed to imports"

--- my-nextjs-app/src/app/index.js
+++ my-nextjs-app/src/app/index.js
@@ -1,10 +1,10 @@
+/** @jsxImportSource @emotion /react @jsxRuntime classic @useClient */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/*import './index.css';*/
 import './styles/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ShelfProvider } from './components/ShelfContext';
+import { ShelfProvider } from './contexts/ShelfContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
