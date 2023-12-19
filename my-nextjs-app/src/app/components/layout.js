915793d27f1644895c4144a0c8cc5f92f375ba7b
ma-boles <ma_boles@hotmail.com>
2023-12-18 19:33:44 -0800

    git commit -m "new layout file"

---
+++ my-nextjs-app/src/app/components/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from 'next/font/google'
+import Footer from './Footer'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Reads',
+  description: 'Keep track of your monthly Spotify audiobooks',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      {children}
+      <Footer />
+        </body>
+
+    </html>
+  )
+}
