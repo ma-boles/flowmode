ee4437ff0056a4bbeee00fc33abbfd02a60be888
ma-boles <ma_boles@hotmail.com>
2023-12-18 19:22:18 -0800

    git commit -m "combined to create HomeContent"

--- my-nextjs-app/src/app/components/Landing.js
+++ my-nextjs-app/src/app/components/Landing.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Logo from '../images/LandingImg5.jpg'
 
 export default function Landing() {
     return (
@@ -7,7 +6,8 @@ export default function Landing() {
         <h2 className="text-right text-lg text-black font-bold">Spotify Reads</h2>
 
         <section className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center">
-            <img src={Logo} alt="astronaut reading a book" className="w-full h-auto"/>
+            <img 
+            src="/LandingImg5.jpg" alt="astronaut reading a book" className="w-full h-auto" />
         </section>
         </>
     )
