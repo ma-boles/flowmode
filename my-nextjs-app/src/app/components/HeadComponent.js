c6447554e543714e442e27541ab184f351677b57
ma-boles <ma_boles@hotmail.com>
2023-12-19 15:48:30 -0800

    git commit -m "created head component"

---
+++ my-nextjs-app/src/app/components/HeadComponent.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function HeadComponent({ metadata }) {
+    return (
+        <Head>
+            <meta charSet='utf-8' />
+            <meta name='viewport' content='width=device-width, intial-scale=1' />
+            <title>{metadata.title}</title>
+            <meta name='description' content={metadata.description} />
+        </Head>
+    )
+}
+
