055295de9ed014e4c1a040e2be6d95d61d1396a3
ma-boles <ma_boles@hotmail.com>
2023-12-19 17:58:03 -0800

    git commit -m "updated imports"

--- my-nextjs-app/src/app/components/HeadComponent.js
+++ my-nextjs-app/src/app/components/HeadComponent.js
@@ -4,10 +4,10 @@ export default function HeadComponent({ metadata }) {
     return (
         <Head>
             <meta charSet='utf-8' />
-            <meta name='viewport' content='width=device-width, intial-scale=1' />
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
             <title>{metadata.title}</title>
             <meta name='description' content={metadata.description} />
         </Head>
-    )
-}
+    );
+};
 
--- my-nextjs-app/src/app/components/layout.js
+++ my-nextjs-app/src/app/components/layout.js
@@ -1,22 +1,23 @@
-import { Inter } from 'next/font/google'
-import Footer from './Footer'
+import React from 'react';
+import HeadComponent from './HeadComponent';
+import Footer from './Footer';
 import './globals.css'
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Spotify Reads',
-  description: 'Keep track of your monthly Spotify audiobooks',
-}
+const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({ children }) {
+export default function Layout ({ metadata, children }) {
+
   return (
     <html lang="en">
+      <HeadComponent metadata={metadata}/>
       <body className={inter.className}>
-      {children}
-      <Footer />
-        </body>
+        {children}
+        <Footer />
+      </body>
 
     </html>
-  )
-}
+
+  );
+};
