1bfb2f7f6037b594b18a2fb1aa1775ea72ee690d
ma-boles <ma_boles@hotmail.com>
2023-12-23 15:48:04 -0800

    git commit -m "moved file to new folder"

---
+++ my-nextjs-app/src/app/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import HeadComponent from './components/HeadComponent';
+import Footer from './components/Footer';
+import '../styles/globals.css';
+import { Inter } from 'next/font/google';
+
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Layout ({ metadata, children }) {
+
+  return (
+    <>
+      <html lang="en">
+        <HeadComponent metadata={metadata}/>
+        <body className={inter.className}>
+          {children}
+          <Footer />
+        </body>  
+      </html>
+    </>
+
+
+  );
+};
