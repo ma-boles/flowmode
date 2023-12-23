50bef7fe8a6c7fe4f7d292de999b44519d66f2d9
ma-boles <ma_boles@hotmail.com>
2023-12-22 17:26:47 -0800

    git commit -m "code set to Home page"

--- my-nextjs-app/src/app/index.js
+++ my-nextjs-app/src/app/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+{/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css';
 import App from 'next/app';
@@ -15,8 +15,6 @@ root.render(
       </Layout>
     </ShelfProvider>
   </React.StrictMode>
-
-
 );
 
 
@@ -24,7 +22,30 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/}
+
+import React from 'react';
+import { ShelfProvider } from './contexts/ShelfContext';
+import HeadComponent from '../components/HeadComponent';
+import Layout from '../components/Layout';
+import HomeContent from '../components/HomeContent'
+
+export default function Home() {
+
+  const homeMetadata = {
+    title: 'Home | Spotify Reads',
+    description: 'Welcome to Spotify Reads - Keep track of your monthly Spotify audiobooks.'
+  };
+
+  return (
+    <ShelfProvider>
+      <Layout metadata={homeMetadata}>
+        <HeadComponent metadata={homeMetadata} />
+        <HomeContent />
+      </Layout>
+    </ShelfProvider>
+  );
+};
 
 
 
