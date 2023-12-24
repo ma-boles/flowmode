7bb3dfe038ed6f742ba70603dfabbdeef76fa588
ma-boles <ma_boles@hotmail.com>
2023-12-23 17:30:16 -0800

    git commit -m "updated layout import"

--- my-nextjs-app/src/app/shelf/page.js
+++ my-nextjs-app/src/app/shelf/page.js
@@ -1,6 +1,7 @@
+// use client
 import React from "react";
 import HeadComponent from "../components/HeadComponent";
-import Layout from "../components/Layout";
+import Layout from "../Layout";
 import ShelfContent from "../components/ShelfContent";
 
 export default function Shelf () {
