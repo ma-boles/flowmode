f04b79b212f0eda033508183c4990c2346a3a71b
ma-boles <ma_boles@hotmail.com>
2024-01-15 17:16:05 -0800

    git commit -m "removed imports"

--- my-nextjs-app/src/app/browse/page.js
+++ my-nextjs-app/src/app/browse/page.js
@@ -1,21 +1,19 @@
 import React from "react";
-import Layout from "../Layout";
 import '../styles/styles.css';
 import '../styles/globals.css';
-import HeadComponent from "../components/HeadComponent";
-import BrowseContent from "../components/BrowseContent";
 
 export default function Browse () {
 
-    const browseMetadata = {
+    const metadata = {
         title: 'Browse | Spotify Reads',
-        description: 'Choose from thousands of titles in the Spotify audiobook collection.'
+        description: 'Choose from thousands of titles in the Spotify audiobook collection'
     }
-
-    return (
-        <Layout metadata={browseMetadata}>
-            <HeadComponent metadata={browseMetadata}/>
-            <BrowseContent />
-        </Layout>
-    );
+    
+    return(
+        <div>
+            <section className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center">
+                <img src="/Browse2.jpg" alt="stack of books" className="w-full h-auto"></img>
+            </section>
+        </div>
+    )
 };
