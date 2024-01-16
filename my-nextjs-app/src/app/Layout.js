628685e26302628f54775bdf100c15a59b32bf78
ma-boles <ma_boles@hotmail.com>
2024-01-16 15:41:56 -0800

    git commit -m "updated imports"

--- my-nextjs-app/src/app/Layout.js
+++ my-nextjs-app/src/app/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import HeadComponent from './components/HeadComponent';
-import Footer from './components/Footer';
+import HeadComponent from '../components/HeadComponent';
+import Footer from '../components/footer/Footer';
 import './styles/globals.css';
 import { Inter } from 'next/font/google';
 
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Layout ({ metadata, children }) {
+export default function RootLayout ({ metadata, children }) {
 
   return (
     <>
