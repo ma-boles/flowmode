20264131facde5a98af5366f06b03549f8c8bfc3
ma-boles <ma_boles@hotmail.com>
2024-01-16 18:02:42 -0800

    git commit -m "added ShlfContext, adjusted logic"

--- my-nextjs-app/src/components/book/Book.js
+++ my-nextjs-app/src/components/book/Book.js
@@ -1,25 +1,54 @@
 import React from "react";
+import { useShelfContext } from "@/app/contexts/ShelfContext";
 
 
-export default function Book({ book, isEllipsisVisible }) {
+export default function Book({ isEllipsisVisible }) {
+
+    const { shelves } = useShelfContext();
 
     const ellipsisStyles = {
         display: isEllipsisVisible ? 'block' : 'none',
     };
 
+    const allBooks = Object.keys(shelves).flatMap((shelfName) => shelves[shelfName]);
+
+    if(allBooks.length === 0) {
+        return 
+    }
+
     return (
         <div>
             <div className="inline text-center">
-                <div className="mt-4 mr-4 ml-4 text-center w-48 h-72 rounded-lg book--card">
+                {allBooks.map((book) => (
+                    <div key={book.id} className="mt-4 mr-4 ml-4 text-center w-48 h-72 rounded-lg book--card">
                     <img src="/image-solid.svg" alt="book cover" className="mx-auto h-40"></img>
                     <div className="text-left mx-4">
-                        {book.title}<br/>
-                        {book.author}<br/><br/>
-                        <strong><p className="book--p--time">{book.book_time}</p></strong>
+                    {book.title && <> {book.title}<br/></>}
+                    {book.author && <>{book.author}<br/><br/></>}
+                    <strong><p className="book--p--time">{book.book_time}</p></strong>
                     </div>
                 </div>
+              ))}
                 <br/>
+                
             </div>
         </div>
-    )
-}
+    );
+};
+
+/*return (
+    <div>
+        <div className="inline text-center">
+            <div className="mt-4 mr-4 ml-4 text-center w-48 h-72 rounded-lg book--card">
+                <img src="/image-solid.svg" alt="book cover" className="mx-auto h-40"></img>
+                <div className="text-left mx-4">
+                    {book.title}<br/>
+                    {book.author}<br/><br/>
+                    <strong><p className="book--p--time">{book.book_time}</p></strong>
+                </div>
+            </div>
+            <br/>
+        </div>
+    </div>
+)
+}*/
