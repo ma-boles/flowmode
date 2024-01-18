cb4a129f09aa6847359012a57891a1f6bda4d987
ma-boles <ma_boles@hotmail.com>
2024-01-18 15:54:05 -0800

    git commit -m "moved all content to new file"

--- spotify-reads/public/indexhtml.js
+++ spotify-reads/public/indexhtml.js
@@ -46,7 +46,7 @@
   </body>
 </html>*/
 
-{/*import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css';
 import App from 'next/app';
@@ -70,9 +70,9 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();*/}
+reportWebVitals();*/
 
-{/*import React from 'react';
+/*import React from 'react';
 import { ShelfProvider } from './contexts/ShelfContext';
 import HeadComponent from '../components/HeadComponent';
 import Layout from './layout';
@@ -93,4 +93,62 @@ export default function Home() {
       </Layout>
     </>
   );
-};*/}
+};*/
+
+/*import Home from './home/page';
+
+
+function App() {
+  return (
+    <div>
+
+    <div /*className='app'*//*> */  
+     /* <Home />
+         
+    </div>
+    </div>
+
+  );
+}
+
+{/*export default App;
+
+import React from "react";
+import { ShelfProvider } from "../contexts/ShelfContext";
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <ShelfProvider>
+            <Component {...pageProps} />
+        </ShelfProvider>
+    );
+};*/
+/*import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});*/
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+//import '@testing-library/jest-dom';
+
+/*const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
+*/
