5913847f40cc8c70aae5acac028b77385bbe6d3c
ma-boles <ma_boles@hotmail.com>
2024-01-23 17:22:53 -0800

    git commit -m"updated img size"

--- my-nextjs-app/src/components/book/Book.js
+++ my-nextjs-app/src/components/book/Book.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ShelfContext, useShelfContext } from "@/app/contexts/ShelfContext";
-import EllipsisButton from "./EllipsisButton";
 
 
 export default function Book({ isEllipsisVisible, shelfName }) {
@@ -23,32 +22,15 @@ export default function Book({ isEllipsisVisible, shelfName }) {
             <div className="inline-flex">
                 {shelfBooks.map((book) => (
                     <div key={book.id} className="mt-4 mr-4 ml-4 text-center w-18 h-72 rounded-lg book--card">
-                    <img src="/image-solid.svg" alt="book cover" className="mx-auto h-40"></img>
+                    <img src="/image-solid.svg" alt="book cover" className="mx-auto p-2 h-40"></img>
                     <div className="text-left mx-4">
                     {book.title && <> {book.title}<br/></>}
                     {book.author && <>{book.author}<br/><br/></>}
                     <strong><p className="book--p--time">{book.book_time}</p></strong>
                     </div>
-                </div>
+                    </div>
               ))}
                 <br/>
             </div>
     );
 };
-
-/*return (
-    <div>
-        <div className="inline text-center">
-            <div className="mt-4 mr-4 ml-4 text-center w-48 h-72 rounded-lg book--card">
-                <img src="/image-solid.svg" alt="book cover" className="mx-auto h-40"></img>
-                <div className="text-left mx-4">
-                    {book.title}<br/>
-                    {book.author}<br/><br/>
-                    <strong><p className="book--p--time">{book.book_time}</p></strong>
-                </div>
-            </div>
-            <br/>
-        </div>
-    </div>
-)
-}*/
