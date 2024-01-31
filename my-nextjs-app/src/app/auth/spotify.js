f81130feb89cac9454882c6424dc3f2316c315ea
ma-boles <ma_boles@hotmail.com>
2024-01-30 17:57:12 -0800

    git commit -m "added pkce generator"

--- my-nextjs-app/src/app/auth/spotify.js
+++ my-nextjs-app/src/app/auth/spotify.js
@@ -1,12 +1,27 @@
-import { signIn } from 'next-auth/react';
+import { encodeVerifier, encodeChallenge } from 'pkce-challenge';
+import { providers } from 'next-auth/providers';
 
-const SpotifyAuthPage = () => {
-    // trigger spotify authentication
-    signIn('spotify');
 
-    //this page won't be rendered since use will be redirected to spotify
-
-    return null;
+const generatePkcePair = () => {
+    const verifier = encodeVerifier();
+    const challenge = encodeChallenge(verifier);
+    return { verifier, challenge };
 };
 
-export default SpotifyAuthPage;
+export default providers.Spotify({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    state: true,
+    pkce: true,
+    authorizationUrl: 'https://accounts.spotify.com/authorize',
+    pkceMethod: 'S256',
+    getAuthorizationUrl: (params) => {
+        const pkcePair = generatePkcePair();
+        return {
+            ...params,
+            code_challenge: pkcePair.challenge,
+            code_challenge_method: 'S256',
+            code_verifier: pkcePair.verifier,
+        };
+    },
+});
