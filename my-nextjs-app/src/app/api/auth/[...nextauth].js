ae9cc94414a82ca1118a3a52166a77e10316acae
ma-boles <ma_boles@hotmail.com>
2024-02-01 18:05:24 -0800

    git commit -m "removed redirectUri"

--- my-nextjs-app/src/app/api/auth/[...nextauth].js
+++ my-nextjs-app/src/app/api/auth/[...nextauth].js
@@ -6,16 +6,11 @@ export default NextAuth ({
         Providers.Spotify({
             clientId: process.env.SPOTIFY_CLIENT_ID,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-            redirectUri: 'https://localhost:3000/api/auth/callback/spotify',
         }),
     ],
     callbacks: {
         async redirect(url, baseUrl) {
-            // log the redirect info 
-            console.log('Redirecting to:', url);
-
-            // redirect logic
-            return Promise.resolve('/shelf');
+            return '/shelf';
         },
     },
 });
