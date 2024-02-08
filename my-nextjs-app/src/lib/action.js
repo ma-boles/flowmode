5c5ed44d3582e84b7ec9b4943052e037a7892ae3
ma-boles <ma_boles@hotmail.com>
2024-02-08 14:16:50 -0800

    git commit -m "removed file - changed auth method"

--- my-nextjs-app/src/lib/action.js
+++
@@ -1,8 +0,0 @@
-"use server";
-
-/*import { signIn } from "@/app/api/auth/[...nextauth]";*/
-import signIn from "next-auth/react";
-
-export const handleLoginClick = async () => {
-    await signIn("spotify");
-};
