3d2f5c0923e2eea2bf39027de295bc68f49606eb
ma-boles <ma_boles@hotmail.com>
2024-02-12 15:59:21 -0800

    git commit m "added refresh token and custom auth"

--- my-nextjs-app/src/app/api/auth/[...nextauth]/options.js
+++ my-nextjs-app/src/app/api/auth/[...nextauth]/options.js
@@ -1,12 +1,15 @@
 import SpotifyProvider from "next-auth/providers/spotify";
 import spotifyApi from "@/app/lib/spotify";
+import { customAuthorizationLogic } from "@/app/lib/auth";
 
 const options = {
     providers: [
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-            scopes: 'user-read-private playlist-read-private playlist-modify-private playlist-modify-public'
+            scopes: 'user-read-private playlist-read-private playlist-modify-private playlist-modify-public',
+            refreshToken: true, // automatically refreshes token
+            customAuthorization: customAuthorizationLogic,
         }),
     ],
     callbacks:/* {
