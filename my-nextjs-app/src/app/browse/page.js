9fa8b260bdd4508d356dbbc4b2cf97114e70ff08
ma-boles <ma_boles@hotmail.com>
2024-02-27 17:40:24 -0800

    git commit -m "removed Link/added to UserProfile"

--- my-nextjs-app/src/app/browse/page.js
+++ my-nextjs-app/src/app/browse/page.js
@@ -1,6 +1,5 @@
 'use client'
 import React from "react";
-import Link from "next/link";
 import UserProfile from "@/components/UserProfile";
 import '../styles/styles.css';
 
@@ -9,12 +8,9 @@ const Browse = () => {
 
 return(
     <div>
-        <Link href="/shelf"><li>Back</li></Link>
+        <UserProfile />
         <section className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center">
             <img src="/Browse2.jpg" alt="stack of books" className="w-full h-auto"></img>
-
-            <UserProfile />
-
         </section>
     </div>
     );
