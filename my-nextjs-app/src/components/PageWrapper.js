0ba60f99ca8611e787a6780a7665ab00c0dedb4f
ma-boles <ma_boles@hotmail.com>
2024-02-27 16:52:52 -0800

    git commit -m "new file importing SessionProvider"

---
+++ my-nextjs-app/src/components/PageWrapper.js
@@ -0,0 +1,11 @@
+import { SessionProvider } from "next-auth/react";
+
+const PageWrapper = ({ children, session }) => {
+    return (
+    <SessionProvider session={session}>
+        {children}
+    </SessionProvider>
+    );
+};
+
+export default PageWrapper;
