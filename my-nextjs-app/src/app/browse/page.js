4512fa0f03fb06ad4b3a78ddca3211f69192573a
ma-boles <ma_boles@hotmail.com>
2024-02-27 17:08:42 -0800

    git commit -m "removed unused imports"

--- my-nextjs-app/src/app/browse/page.js
+++ my-nextjs-app/src/app/browse/page.js
@@ -1,38 +1,11 @@
 'use client'
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import UserProfile from "@/components/UserProfile";
-import { getSession } from "next-auth/react";
 import '../styles/styles.css';
-import makeApiRequest from "../lib/spotifyApi";
-import { useFetchUserData } from "@/components/UserProfile";
 
- // Add the getSession function in getProps
- export async function getProps(context) {
-    const session = await getSession(context);
-    return {
-        props: { session },
-    };
-}
 
-const Browse = ({ session }) => {
-    console.log('Session:', session);
-    const [account, setAccount] = useState(null);
-    console.log('Account:', account);
-
-    const metadata = {
-        title: 'Browse | Spotify Reads',
-        description: 'Choose from thousands of titles in the Spotify audiobook collection'
-    }
-    
-    useEffect(() => {
-        if(session) {
-            setAccount(session.account);
-        }
-    }, [session]);
-
-    console.log('Session:', session);
-    console.log('Account:', account);
+const Browse = () => {
 
 return(
     <div>
@@ -40,7 +13,7 @@ return(
         <section className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center">
             <img src="/Browse2.jpg" alt="stack of books" className="w-full h-auto"></img>
 
-            <UserProfile account={session?.account}/>
+            <UserProfile />
 
         </section>
     </div>
