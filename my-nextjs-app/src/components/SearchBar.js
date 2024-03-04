26a665eea1e7fded718d30f7ede009e3da56cb7a
ma-boles <ma_boles@hotmail.com>
2024-03-04 15:53:05 -0800

    git commit -m "new component file"

---
+++ my-nextjs-app/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+export default function SearchBar() {
+    return (
+            <div className="shadow-xl shadow-slate-900">
+                <div className="bg-white flex items-center justify-center  rounded-md">
+                    <div>
+                        <input className="outline-none p-2 m-4 text-xl text-slate-800 	" type="text" placeholder="What do I want to listen to?"></input>
+                    </div>
+                    <button className="p-3 mr-4 pl-10 pr-10 bg-green-500 rounded-md">Search</button>
+                </div>
+            </div>
+    )
+}
