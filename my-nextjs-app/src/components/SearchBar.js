c20cd949041b6b4c56e70b0e91eb0abb15b872ef
ma-boles <ma_boles@hotmail.com>
2024-03-04 17:47:45 -0800

    git commit -m "added styling"

--- my-nextjs-app/src/components/SearchBar.js
+++ my-nextjs-app/src/components/SearchBar.js
@@ -6,7 +6,7 @@ export default function SearchBar() {
             <div className="shadow-xl shadow-slate-900">
                 <div className="bg-white flex items-center justify-center  rounded-md">
                     <div>
-                        <input className="outline-none p-2 m-4 text-xl text-slate-800 	" type="text" placeholder="What do I want to listen to?"></input>
+                        <input className="outline-none p-2 m-4 text-xl text-slate-800 	" type="text" placeholder="Title, Artist, Name..."></input>
                     </div>
                     <button className="p-3 mr-4 pl-10 pr-10 bg-green-500 rounded-md">Search</button>
                 </div>
