5724915100338d395337245cdcb8263aa3e5df67
ma-boles <ma_boles@hotmail.com>
2024-03-05 18:02:02 -0800

    git commit -m "added hover to buttons"

--- my-nextjs-app/src/app/browse/page.js
+++ my-nextjs-app/src/app/browse/page.js
@@ -12,20 +12,19 @@ const Browse = () => {
 return(
     <div>
         {/*<UserProfile />*/}
-        <div className="mt-6 flex justify-around">
+        <div className="mt-4 flex justify-around">
                 <Link href="./shelf">
-                    <button className="font-semibold">Dashboard</button>
+                    <button className="p-2 px-4 text-white font-semibold  rounded-md hover:bg-blue-700  transition duration-300 ease-in-out">Dashboard</button>
                 </Link>
-                <button className="font-semibold">Music</button>
-                <button className="font-semibold">Podcasts</button>
-                <button className="font-semibold">Books</button>
+                <button className="p-2 px-4 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Music</button>
+                <button className="p-2 px-4 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Podcasts</button>
+                <button className="p-2 px-4 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Books</button>
             </div>
         <section className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center">
-        <div className="p-20 border-solid border-slate-100 border-2">
-            <h2 className="pb-12 font-semibold text-center text-xl">What would you like to listen to?</h2>
-        
+        <div className="p-20 border-solid border-transparent border-2">
+            <h2 className="pb-12 font-semibold text-center text-5xl">What <span className="font-extrabold text-blue-800">would</span> you like <br/>to <span className="font-extrabold text-blue-800">listen </span> to?</h2>
                 <SearchBar />
-            </div>
+                </div>
         </section>
     </div>
     );
@@ -37,3 +36,4 @@ export default Browse;
 
 /*<img src="/Browse2.jpg" alt="stack of books" className="w-full h-auto"></img>*/
             
+/*className="p-20 border-solid border-slate-100 border-2"*/
