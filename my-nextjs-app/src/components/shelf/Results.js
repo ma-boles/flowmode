95959f077b6d7f947b400fbddbcac16b13dec107
ma-boles <ma_boles@hotmail.com>
2024-03-06 15:40:19 -0800

    git commit -m "new file for search results"

---
+++ my-nextjs-app/src/components/shelf/Results.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Results() {
+    return (
+        <>
+        <section className="bg-white ">
+            <h1 className="mt-0 pt-0 text-slate-900 font-semibold">Results</h1>
+                <p className="text-black">test1</p>
+                <p className="text-black">test2</p>
+        </section>
+        
+        </>
+    )
+}
