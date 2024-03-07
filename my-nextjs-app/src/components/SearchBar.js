b35fb1af73d26f67b48d63075a0edb23105b22cc
ma-boles <ma_boles@hotmail.com>
2024-03-06 18:11:27 -0800

    git commit -m "updated css"

--- my-nextjs-app/src/components/SearchBar.js
+++ my-nextjs-app/src/components/SearchBar.js
@@ -3,12 +3,13 @@ import React from "react"
 
 export default function SearchBar() {
     return (
-            <div className="shadow-xl shadow-slate-900">
-                <div className="bg-white flex items-center justify-center  rounded-md">
+            <div className="shadow-xl shadow-black">
+                
+               <div className="bg-white flex items-center justify-center  rounded-md">
                     <div>
                         <input className="outline-none p-2 m-4 text-xl text-slate-800 	" type="text" placeholder="Title, Artist, Name..."></input>
                     </div>
-                    <button className="p-3 mr-4 pl-10 pr-10 bg-green-500 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Search</button>
+                    <button className="p-3 mr-4 pl-10 pr-10 bg-green-600 rounded-md hover:bg-gray-700 transition duration-300 ease-in-out">Search</button>
                 </div>
             </div>
     )
