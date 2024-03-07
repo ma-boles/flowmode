c4149c91330463da8307093fef1da339cc5e5cc0
ma-boles <ma_boles@hotmail.com>
2024-03-06 18:10:22 -0800

    git commit -m "changed Back button to Dashboard"

--- my-nextjs-app/src/components/UserProfile.js
+++ my-nextjs-app/src/components/UserProfile.js
@@ -11,9 +11,8 @@ const UserProfile = () => {
     }
 
     return(
-        <div className="flex justify-between mx-4">
-            <Link href="/shelf"><li>Back</li></Link>
-
+        <div className="flex px-2 justify-between w-full items-center">
+            <Link href="/shelf"><button className=" p-2 px-4 text-white font-semibold rounded-md hover:bg-green-600  transition duration-300 ease-in-out" >Dashboard</button></Link>
             <p className="mx-4 my-4 font-semibold">Welcome, {session.user.sub}!</p>
             <img src={session.user.picture} alt="User" className="w-20 h-20 p-4 object-cover rounded-full"></img>
         </div>
