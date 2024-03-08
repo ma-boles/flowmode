b421ea9524a3f4762400b113e60683aa412fb181
ma-boles <ma_boles@hotmail.com>
2024-03-07 18:01:15 -0800

    git commit -m "imported dropdown menu"

--- my-nextjs-app/src/components/UserProfile.js
+++ my-nextjs-app/src/components/UserProfile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSession } from "next-auth/react";
+import DropdownMenu from "./DropdownMenu";
 import Link from "next/link";
 
 
@@ -9,14 +10,36 @@ const UserProfile = () => {
     if (!session) {
         return <p>Please log in</p>;
     }
+  
 
     return(
         <div className="flex px-2 justify-between w-full items-center">
-            <Link href="/shelf"><button className=" p-2 px-4 text-white font-semibold rounded-md hover:bg-green-600  transition duration-300 ease-in-out" >Dashboard</button></Link>
-            <p className="mx-4 my-4 font-semibold">Welcome, {session.user.sub}!</p>
+          {/*  <div className="dropdown">
+                <div className="relative inline-block spx-2 py-1 border border-single bg-transparent rounded-md">
+                    <button className="bg-gray-600">nav</button>
+                    <li className="bg-gray-600" value="dashboard">Dashboard</li>
+
+                    <li className="bg-gray-600" value="browse">
+                        <Link href="/browse">Browse</Link></li>
+                    
+                    <li className="bg-gray-600" value="music">Music</li>
+                    <li className="bg-gray-600" value="podcasts">Podcasts</li>
+
+                    <Link href="/shelf">
+                    <li className="bg-gray-600 rounded-md" value="books">Books</li>
+                    </Link>
+
+                </div>
+                
+    </div>*/}
+            
             <img src={session.user.picture} alt="User" className="w-20 h-20 p-4 object-cover rounded-full"></img>
+            <DropdownMenu />
         </div>
     );
 };
 
+
 export default UserProfile;
+{/*<Link href="/shelf"><button className=" p-2 px-4 text-white font-semibold rounded-md hover:bg-green-600  transition duration-300 ease-in-out" >Dashboard</button></Link>*/}
+            {/*<p className="mx-4 my-4 font-semibold">Welcome, {session.user.sub}!</p>*/}
