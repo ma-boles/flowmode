c2e3f3a1710d0d2013d3d8d7e4e98134b87a14a5
ma-boles <ma_boles@hotmail.com>
2024-03-07 17:59:49 -0800

    git commit -m "dropdown menu component"

---
+++ my-nextjs-app/src/components/DropdownMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "@/app/styles/styles.css"
+
+export default function DropdownMenu () {
+    return (
+        <>
+            <div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  
+</div>
+        </>
+    )
+}
