b3a9dc11d57c3d5f3a703f1818505e1f8d4b410b
ma-boles <ma_boles@hotmail.com>
2024-03-12 18:06:28 -0700

    git commit -m "added try-catch block to functions"

--- my-nextjs-app/src/app/lib/apiCall.js
+++ my-nextjs-app/src/app/lib/apiCall.js
@@ -1,33 +1,50 @@
 import axios from "axios";
 
 const searchArtists = async (query, accessToken) => {
-    const response = await axios.get('https://api.spotify/com/v1/search', {
-        params: {
-            type: 'artist',
-            q: query,
-        },
-        headers:{
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    return response.data.artist.items;
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/search', {
+            params: {
+                type: 'artist',
+                q: query,
+            },
+            headers:{
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        console.log('Artist search response:', response.data);
+        return response.data.artists.items;
+
+    } catch (error) {
+        console.log('Error searching artist:', error);
+        throw error;
+    }
 };
 
 const searchAlbums = async (query, accessToken) => {
-    const response = await axios.get('https://api.spotify/com/v1/search', {
-        params: {
-            type: 'album',
-            q: query,
-        },
-        headers:{
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    return response.data.albums.items;
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/search', {
+            params: {
+                type: 'album',
+                q: query,
+            },
+            headers:{
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        console.log('Album search response:', response.data);
+        return response.data.albums.items;
+
+    } catch(error) {
+        console.log('Error searching alum:', error);
+        throw error;
+    }
 };
 
 const searchTrack = async (query, accessToken) => {
-    const response = await axios.get('https://api.spotify/com/v1/search', {
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/search', {
         params: {
             type: 'track',
             q: query,
@@ -35,47 +52,79 @@ const searchTrack = async (query, accessToken) => {
         headers:{
             Authorization: `Bearer ${accessToken}`,
         },
-    });
+    })
+
+    console.log('Track search response:', response.data);
     return response.data.tracks.items;
+
+    } catch (error) {
+        console.log('Error searching track:', error);
+        throw error;
+    }
 };
 
 const searchAudiobooks = async (query, accessToken) => {
-    const response = await axios.get('https://api.spotify/com/v1/search', {
-        params: {
-            type: 'audiobook',
-            q: query,
-        },
-        headers:{
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    return response.data.audiobooks.items;
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/search', {
+            params: {
+                type: 'audiobook',
+                q: query,
+            },
+            headers:{
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        console.log('Audiobook search response:', response.data);
+        return response.data.audiobooks.items;
+
+    } catch (error) {
+        console.log('Error searching audiobook:', error);
+        throw error;
+    }
 };
 
 const searchPodcastShow = async (query, accessToken) => {
-    const response = await axios.get('https://api.spotify/com/v1/search', {
-        params: {
-            type: 'show',
-            q: query,
-        },
-        headers:{
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    return response.data.shows.items;
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/search', {
+            params: {
+                type: 'show',
+                q: query,
+            },
+            headers:{
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        console.log('Show search response:', response.data);
+        return response.data.shows.items;
+
+    } catch (error) {
+        console.log('Error searching show:', error);
+        throw error;
+    }
 };
 
 const searchPodcastEpisode = async (query, accessToken) => {
-    const response = await axios.get('https://api.spotify/com/v1/search', {
-        params: {
-            type: 'episode',
-            q: query,
-        },
-        headers:{
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    return response.data.episodes.items;
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/search', {
+            params: {
+                type: 'episode',
+                q: query,
+            },
+            headers:{
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        console.log('Show search response:', response.data);
+        return response.data.episodes.items;
+
+    } catch (error) {
+        console.log('Error searching episode:', error);
+        throw error;
+    }
 };
+
 
 export { searchArtists, searchTrack, searchAlbums, searchAudiobooks, searchPodcastShow, searchPodcastEpisode  };
