af35734fbff78db9d6cf339c53b24b9b4afed24f
ma-boles <ma_boles@hotmail.com>
2024-03-12 18:07:25 -0700

    git commit -m "added mapping for artist"

--- my-nextjs-app/src/components/browse/Results.js
+++ my-nextjs-app/src/components/browse/Results.js
@@ -1,14 +1,26 @@
 import React from "react";
 
-export default function Results() {
+export default function Results({ artists }) {
+
+    /*if(!searchResults) {
+        return <div>Loading...</div>;
+    }*/
+
     return (
         <>
         <section className="bg-white ">
             <h1 className="mt-0 pt-0 text-slate-900 font-semibold">Results</h1>
                 <p className="text-black">test1</p>
                 <p className="text-black">test2</p>
+                <ul>
+                    {artists.map(artist => (
+                        console.log('Album:', artist),
+                        <li key={artist.id}>
+                            <h3>{artist.name}</h3>
+                        </li>
+                    ))}
+                </ul>
         </section>
-        
         </>
     )
 }
