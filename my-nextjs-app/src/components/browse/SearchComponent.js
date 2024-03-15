9bd49c4a884df8942ddef8d5ddc2aadf6e8cd238
ma-boles <ma_boles@hotmail.com>
2024-03-14 18:06:35 -0700

    git commit -m "added cards - artist, album, song"

--- my-nextjs-app/src/components/browse/SearchComponent.js
+++ my-nextjs-app/src/components/browse/SearchComponent.js
@@ -8,8 +8,10 @@ export default function SearchComponent () {
 
     const { data: session } = useSession();
     const accessToken = session?.accessToken;
+    const [searchResults, setSearchResults] = useState([]);
     const [category, setCategory] = useState('');
     const [keyword, setKeyword] = useState('');
+    
 
     const handleSearch = async () => {
         console.log('Search initiated with category:', category, 'and keyword:', keyword );
@@ -51,7 +53,7 @@ export default function SearchComponent () {
     };
 
     return (
-            <div className="bg-transparent ">
+            <div className="bg-transparent">
                 <div className="pb-12 flex justify-center">
                     <select className="p-2 px-4 bg-green-600 font-medium text-lg rounded-md"
                     value={category}
@@ -63,37 +65,65 @@ export default function SearchComponent () {
                         <option value="audiobook">Book</option>
                         <option value="show">Podcast Show</option>
                         <option value="episode">Podcast Episode</option>
-
                     </select>
                 </div>
 
-               <div className="bg-white rounded-md">
-                    <input className="outline-none p-2 m-4 text-xl text-slate-800"
-                    type="text"
-                    placeholder="Title, Artist, Name..."
-                    value={keyword}
-                    onChange={(e) => setKeyword(e.target.value)}
-                    />
-
-                    <button className="p-3 mr-4 pl-10 pr-10 bg-green-600 rounded-md hover:bg-gray-700 transition duration-300 ease-in-out"
-                    onClick={handleSearch}
-                    >Search</button>
+                <div className="flex justify-center">
+                <div className="bg-white rounded-md input">
+                        <input className="outline-none p-2 m-4 text-xl text-slate-800"
+                        type="text"
+                        placeholder="Title, Artist, Name..."
+                        value={keyword}
+                        onChange={(e) => setKeyword(e.target.value)}
+                        />
+                        <button className="p-3 mr-4 pl-10 pr-10 bg-green-600 rounded-md hover:bg-gray-700 transition duration-300 ease-in-out"
+                        onClick={handleSearch}
+                        >Search</button>
+                    </div>
                 </div>
+                <div className="mt-8 w-full ">
 
-                <div className="mx-2 bg-blue-500 ">
-                    <h1 className="mt-0 pt-0 text-slate-900 font-semibold">Results</h1>
+                {category === 'artist' &&
+                    <div className="flex flex-wrap justify-center">
                         {searchResults.map((artist, index) => (
-                            <ul key={index}>
-                                <li>{artist.name}</li>
-                                <li>{artist.genre}</li>
+                            <ul key={index} className="justify-center m-1 bg-red-500 flex flex-col items-center rounded-md artistCard">
+                                <li className="text-center">
+                                {artist.images[2] && (
+                                    <img src={artist.images[2].url} alt={`Image of ${artist.name}`} className="artistImg"/>
+                                    )}
+                                    <h2 className="font-bold">{artist.name}</h2>
+                                    <p>{artist.genres.join(', ')}</p>
+                                </li>
                             </ul>
                         ))}
+                    </div>}
+
+                    {category === 'album' && 
+                        <div className="flex flex-wrap justify-center">
+                            {searchResults.map ((album, index) => (
+                                <ul key={index} className="justify-center m-1 bg-red-500 flex flex-col items-center rounded-md artistCard">
+                                    {album.images[2] && (
+                                        <img src={album.images[2].url} alt={`Image of ${album.name}`} className="artistImg"></img>
+                                    )}
+                                    <h2 className="font-semibold text-center">{album.name}</h2>
+                                    <h2>{album.artists[0].name}</h2>
+                                </ul>
+                            ))}
+                        </div>}
+                    
+                    {category === 'track' && 
+                        <div className="flex flex-wrap justify-center">
+                            {searchResults.map ((track, index) => (
+                                <ul key={index} className="justify-center m-1 flex flex-col items-center rounded-md artistCard">
+                                    <img src={track.album.images[2].url} alt={`Album cover of ${track.album.name}`} className="trackImg" />
+
+                                    <h2 className="text-center font-bold">{track.name}</h2>
+                                    <h2 className="text-center">{track.artists[0].name}</h2>
+                                </ul>
+                            ))}
+                        </div>
+                    }
                 </div>
             </div>
-            
-            
-            
     )
 }
-
-                
