1bbd4ad9bac3b6c9a1e8ab1a7f07dc2c087a5fd2
ma-boles <ma_boles@hotmail.com>
2024-03-18 17:55:36 -0700

    git commit -m "imported playlist display"

--- my-nextjs-app/src/app/dashboard/page.js
+++ my-nextjs-app/src/app/dashboard/page.js
@@ -1,9 +1,11 @@
 'use client'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserProfile from "@/components/UserProfile";
+import Display from "@/components/playlists/Display";
 import Link from "next/link";
 
 export default function Dashboard() {
+
     return (
         <>
             <nav>
@@ -11,7 +13,6 @@ export default function Dashboard() {
             </nav>
 
             < div className="mx-12 ">
-                
                     <div className="flex justify-center">
                         <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Music</button>
                         <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Podcasts</button>
@@ -21,15 +22,16 @@ export default function Dashboard() {
                     </div>
                     <div className="flex justify-center">
                         <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">My<br/>Playlists</button>
+
                         <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Followed<br/>Playlists</button>
+
                         <Link href="/browse">
                             <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Browse <br/> &rarr;</button>
                         </Link>
                     </div>
-                
+                    <Display />
+
             </div>
-           
         </>
-        
     )
 }
