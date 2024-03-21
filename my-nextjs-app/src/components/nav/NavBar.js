eec6f3c57e5217b4b8329da6ec23f94afa359b53
ma-boles <ma_boles@hotmail.com>
2024-03-21 15:21:22 -0700

    git commit -m "renamed and moved to nav folder"

--- my-nextjs-app/src/components/nav/NavBar.js
+++ my-nextjs-app/src/components/nav/NavBar.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Link from 'next/link';
 import '@/app/styles/styles.css'
 
-export default function Nav({ scrollToRef }) {
+export default function NavBar({ accessToken, scrollToRef }) {
+
+    /*const accessToken = accessToken;*/
 
     function changeText() {
         var timeRemaining = document.getElementById('time');
@@ -14,6 +16,7 @@ export default function Nav({ scrollToRef }) {
             <section className='flex justify-between my-0 mx-4 w-full'>
                 <div className='mt-4 ml-4'>
                     <p className='mt-2 ml-4 font-bold text-base'>Spotify Reads</p>
+                    {/*<UserProfile accessToken={accessToken}/>*/}
                 </div>
                 <div className='links--section'>
                     <ul className='mr-8 list-none'>
@@ -22,6 +25,7 @@ export default function Nav({ scrollToRef }) {
                         <li onClick={() => scrollToRef('ReadingList')}>Reading List</li>
 
                         <Link href="/browse"><li>Browse</li></Link>
+                        <Link href="/dashboard"><li>Dashboard</li></Link>
 
                         <li className='p-1 text-center text-base bg-transparent w-20 border-solid border-2 border-neutral-100 cursor-pointer' id='time' onClick={changeText}>
                             <p className=' text-sm'>Time Left</p></li>
