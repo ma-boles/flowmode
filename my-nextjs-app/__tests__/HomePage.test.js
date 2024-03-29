bea342b216551cecc5a0056dccf04442f0a0d215
ma-boles <ma_boles@hotmail.com>
2024-03-28 18:18:27 -0700

    git commit -m "updated test"

--- my-nextjs-app/__tests__/HomePage.test.js
+++ my-nextjs-app/__tests__/HomePage.test.js
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import HomePage from '@/app/page'
 
-it('should have Reads text', () => {
-    render(<HomePage />)
+describe('HomePage', () => {
+    it('renders How it works text', () => {
+        render(<HomePage />)
+        const myElem = screen.getByText('How it works')
+        expect(myElem).toBeInTheDocument()
+     });
 
-    const myElem = screen.getByText('Reads')
-
-    expect(myElem).toBeInTheDocument()
-})
+});
