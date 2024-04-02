4bf99b284280ec9a1d51919a84289551c8abfe07
ma-boles <ma_boles@hotmail.com>
2024-04-02 15:18:41 -0700

    git commit -m "fixed error -- class to className"

--- my-nextjs-app/src/components/nav/DropdownMenu.js
+++ my-nextjs-app/src/components/nav/DropdownMenu.js
@@ -4,8 +4,8 @@ import "@/app/styles/styles.css"
 export default function DropdownMenu () {
     return (
         <>
-            <div class="dropdown">
-  <button class="dropbtn">Dropdown</button>
+            <div className="dropdown">
+  <button className="dropbtn">Dropdown</button>
   
 </div>
         </>
