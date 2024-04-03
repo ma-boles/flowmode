fbda054dffe62041f8f05e3ce695751a2bb18f1f
ma-boles <ma_boles@hotmail.com>
2024-04-02 17:59:38 -0700

    git commit -m "added accessToken mock"

--- my-nextjs-app/__mocks__/next-auth/react.js
+++ my-nextjs-app/__mocks__/next-auth/react.js
@@ -1,14 +1,17 @@
+import { v4 as uuid4 } from 'uuid';
+
 const nextAuthReact = jest.requireActual('next-auth/react');
 
 module.exports = {
     ...nextAuthReact,
-    useSession: () => ({
+    useSession: jest.fn(() => ({
         data: {
             user: {
                 name: 'Mocked User',
                 email: 'mock@example.com',
             },
+            accessToken: uuid4(),
         },
         status: 'authenticated',
-    }),
+    })),
 };
