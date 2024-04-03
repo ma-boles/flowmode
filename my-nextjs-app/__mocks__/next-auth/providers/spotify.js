353c33ee53dfefe7a640ecd211821f6848551c97
ma-boles <ma_boles@hotmail.com>
2024-04-02 18:01:05 -0700

    git commit -m "revised naming of mock provider"

--- my-nextjs-app/__mocks__/next-auth/providers/spotify.js
+++ my-nextjs-app/__mocks__/next-auth/providers/spotify.js
@@ -3,14 +3,14 @@ const { v4: uuidv4 } = require('uuid');
 const mock_client_id = uuidv4();
 const mock_client_secret = uuidv4();
 
-const mockProvider = jest.fn(() => ({
+const mockSpotifyProvider = jest.fn(() => ({
     id: 'spotify',
     name: 'Spotify',
     type: 'oauth',
     version: '2.0',
     scope: 'user-read-email user-read-private playlist-read-private playlist-modify-private playlist-modify-public user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played streaming',
-    clientId: 'mock_client_id',
-    clientSecret: 'mock_client_secret',
+    clientId: mock_client_id,
+    clientSecret: mock_client_secret,
     accessTokenUrl: 'https://accounts.spotify.com/api/token',
     authorizationUrl: 'https://accounts.spotify.com/authorize',
     profileUrl: 'https://api.spotify.com/v1/me',
