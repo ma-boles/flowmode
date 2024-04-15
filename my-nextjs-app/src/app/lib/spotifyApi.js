8cc97acc83b76cb55ed813504ad146ae2fe13bdc
ma-boles <ma_boles@hotmail.com>
2024-04-14 17:49:21 -0700

    git commit -m "added logic to handle 401 errors"

--- my-nextjs-app/src/app/lib/spotifyApi.js
+++ my-nextjs-app/src/app/lib/spotifyApi.js
@@ -11,14 +11,38 @@ const makeApiRequest = async(url, account) => {
                 'Content-Type': 'application/json',
             },
         });
-        
+
+        // Check response status
         if(response.status === 200) {
             return response.data;
         } else {
             throw new Error(`Error making API request: ${response.statusText}`);
         }
     } catch (error) {
-        console.error('Error making API request:', error.message);
+        console.error('Error during API request:', error.message);
+
+        // Handle 401 Unauthorized errors specifically
+        if(error.response && error.response.status === 401) {
+            console.log('Token expired or invalid, attempting to refresh...');
+            const tokens = await getTokens(account);
+
+            // Retry the request with new token
+            const retryResponse = await axios.get(url, {
+                headers: {
+                        'Authorization': `Bearer ${tokens.accessToken}`,
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                // Check rety response and return data
+                if(retryResponse.status === 200) {
+                    return retryResponse.data;
+                } else {
+                    throw new Error(`Error after token refresh: ${retryResponse.statusText}`);
+                }
+        }
+
+        //Rethrow the original error if it is not related invalid token
         throw error;
     }
 };
