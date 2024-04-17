419d7e52ca4cb6b04f31001f826d7c676c700f04
ma-boles <ma_boles@hotmail.com>
2024-04-17 15:44:31 -0700

    git commit -m"conditionally render PlayerProvider"

--- my-nextjs-app/src/components/PageWrapper.js
+++ my-nextjs-app/src/components/PageWrapper.js
@@ -4,11 +4,18 @@ import { SessionProvider } from "next-auth/react";
 import { PlayerProvider } from "@/app/providers/PlayerProvider";
 
 const PageWrapper = ({ children, session }) => {
+    // Check if window is defined (running in the browser)
+    const isBrowser = typeof window !== 'undefined';
+
+    // Determine if the current page is the Browse or Dashboard
+    const isBrowseOrDashboardPage = isBrowser && ['/browse', '/dashboard'].includes(window.location.pathname);
+
+    // Conditionally wrap children with PlayerProvider based on the current page
+    const wrappedChildren = isBrowseOrDashboardPage ? <PlayerProvider>{children}</PlayerProvider> : children;
+
     return (
     <SessionProvider session={session}>
-        <PlayerProvider>
-            {children}
-        </PlayerProvider>
+        {wrappedChildren}
     </SessionProvider>
     );
 };
