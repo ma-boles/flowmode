f20d4b5f7030a828dd9afa794d10d12376a8290a
ma-boles <ma_boles@hotmail.com>
2024-04-18 10:35:08 -0700

    git commit -m "updated date"

--- my-nextjs-app/src/components/footer/Footer.js
+++ my-nextjs-app/src/components/footer/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default function Footer () {
+    const getCurrentYear = () => {
+        return new Date().getFullYear();
+    };
+
     return (
         <div>
             <div>
                 <hr className="mx-auto opacity-60 footer--hr"></hr>
-                <p className="m-8 text-right">&copy; mab.dev 2023 </p>
+                <p className="m-8 text-right">&copy; mboles.dev {getCurrentYear()} </p>
             </div>
         </div>
     )
