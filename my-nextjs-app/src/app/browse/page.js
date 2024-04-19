933b5874bcf39cc3df0aa6f1370add3e1e205ca7
ma-boles <ma_boles@hotmail.com>
2024-04-18 18:14:45 -0700

    git commit -m "removed commented out code"

--- my-nextjs-app/src/app/browse/page.js
+++ my-nextjs-app/src/app/browse/page.js
@@ -51,7 +51,3 @@ return(
 };
 
 export default Browse;
-
-/*className="mx-auto w-full max-w-6xl  min-h-screen h-auto flex items-center justify-center"*/
-
-/*<img src="/Browse2.jpg" alt="stack of books" className="w-full h-auto"></img>*/
