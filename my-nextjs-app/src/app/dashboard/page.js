936c9627ffdc04c75f2290dc7b91d22483d94ca0
ma-boles <ma_boles@hotmail.com>
2024-05-16 17:05:34 -0700

    git commit -m "added stats boxes"

--- my-nextjs-app/src/app/dashboard/page.js
+++ my-nextjs-app/src/app/dashboard/page.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Player from "@/components/Player";
 import { useSession } from "next-auth/react";
 
+
 export default function Dashboard() {
 
     const { data: session } = useSession();
@@ -23,24 +24,31 @@ export default function Dashboard() {
                 <UserProfile />
             </nav>
 
-            < div className="mx-12 ">
-                    <div className="flex justify-center">
-                        <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Music</button>
-                        <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Podcasts</button>
-                        <Link href="/shelf">
-                            <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Books</button>
-                        </Link>
+            <div className="mx-12">
+                <h1 className="p-0 mb-10 text-center">Welcome!</h1>
+                    <div /* top section */ className="flex justify-center">
+                        <div className="px-12 py-6 mb-0 mx-8 border-8 border-solid border-white rounded-lg opacity-50">
+                            <h2 className="m-2 font-bold text-2xl opacity-90">Minutes of <br /> Focused Work:</h2>
+                            <hr />
+                            <h2 className="mx-2 mt-8 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">00</span> MIN</h2>
+                        </div>
+                        <div className="py-6 px-12 mx-8 border-8 border-solid border-white rounded-lg opacity-50">
+                            <h2 className="m-2 font-bold text-2xl opacity-90">Most Recently <br /> Played:</h2>
+                            <hr />
+                            <div className="flex flex-col">
+                                <h2 className="m-2 text-xl font-semibold">Title #1</h2>
+                                <h2 className="m-2 text-xl font-semibold">Title #2</h2>
+                                <h2 className="m-2 text-xl font-semibold">Title #3</h2>
+                            </div>
+                        </div>
+                        <div className="mx-2 flex flex-col justify-center items-center">
+                            <button className="mx-4 mb-4 h-32 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">My<br/>Playlists</button>
+                            <button className="mx-4 mt-4 h-32 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Followed<br/>Playlists</button>
+                        </div>
                     </div>
-                    <div className="flex justify-center">
-                        <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">My<br/>Playlists</button>
 
-                        <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Followed<br/>Playlists</button>
-
-                        <Link href="/browse">
-                            <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Browse <br/> &rarr;</button>
-                        </Link>
-                    </div>
                     {/*<Display />*/}
+
                     <button className="m-8 p-8 bg-green-600" onClick={handleButtonClick}>Open Player</button>
                     {isPlayerOpen && (
                         <Player accessToken={accessToken}/>
@@ -49,3 +57,20 @@ export default function Dashboard() {
         </>
     )
 }
+
+                        {/*<button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Music</button>
+                        <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Podcasts</button>
+                        <Link href="/shelf">
+                            <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Books</button>
+                        </Link>*/}
+
+                        
+                    {/*<div className="flex justify-center">
+                        <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">My<br/>Playlists</button>
+
+                        <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Followed<br/>Playlists</button>
+
+                        <Link href="/browse">
+                            <button className="m-4 h-60 w-80 font-semibold bg-green-600 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Browse <br/> &rarr;</button>
+                        </Link>
+                        </div>*/}
