9407ba00d6ab5e27e236a435f290d76dc6c90cb4
ma-boles <ma_boles@hotmail.com>
2024-05-19 16:01:23 -0700

    git commit -m "added dropdown menu"

--- my-nextjs-app/src/components/profile/UserProfile.js
+++ my-nextjs-app/src/components/profile/UserProfile.js
@@ -26,7 +26,17 @@ const UserProfile = () => {
 
     return(
         <div className="flex px-2 justify-between w-full items-center">
-            <img src={session.user.picture} alt="User" className="w-20 h-20 p-4 object-cover rounded-full"></img>
+            <div className="dropdown">
+            <button className="bg-gray-500 dropdown-btn ">
+                <img src={session.user.picture} alt="User" className="m-auto w-14 h-14 object-cover rounded-full"></img>
+            </button>
+                <div className="border border-solid rounded-md dropdown-content">
+                    <a rel="noopener noreferrer" href="https://open.spotify.com/">
+                        <p className="hover:bg-green-600 transition duration-300 ease-in-out dropdown-link">Spotify</p>
+                    </a>
+                    <p className="border-t border-solid border-gray-600 hover:bg-red-600 transition duration-300 ease-in-out dropdown-link">Log Out</p>
+                </div>
+            </div>
 
             {currentPath === '/browse' && (
                 <Link href='/dashboard'>
@@ -45,24 +55,3 @@ const UserProfile = () => {
 
 
 export default UserProfile;
-{/*<Link href="/shelf"><button className=" p-2 px-4 text-white font-semibold rounded-md hover:bg-green-600  transition duration-300 ease-in-out" >Dashboard</button></Link>*/}
-            {/*<p className="mx-4 my-4 font-semibold">Welcome, {session.user.sub}!</p>*/}
-
-            {/*  <div className="dropdown">
-                <div className="relative inline-block spx-2 py-1 border border-single bg-transparent rounded-md">
-                    <button className="bg-gray-600">nav</button>
-                    <li className="bg-gray-600" value="dashboard">Dashboard</li>
-
-                    <li className="bg-gray-600" value="browse">
-                        <Link href="/browse">Browse</Link></li>
-                    
-                    <li className="bg-gray-600" value="music">Music</li>
-                    <li className="bg-gray-600" value="podcasts">Podcasts</li>
-
-                    <Link href="/shelf">
-                    <li className="bg-gray-600 rounded-md" value="books">Books</li>
-                    </Link>
-
-                </div>
-                
-    </div>*/}
