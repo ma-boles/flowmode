466320f9aedac1bebc4b75c4667656542e89fcfb
ma-boles <ma_boles@hotmail.com>
2024-05-27 16:03:54 -0700

    git commit -m "updated dropdown tailwindcss"

--- my-nextjs-app/src/components/profile/UserProfile.js
+++ my-nextjs-app/src/components/profile/UserProfile.js
@@ -28,10 +28,10 @@ const UserProfile = () => {
         <div className="flex px-8 justify-between w-full bg-neutral-800 items-center">
             <div className="dropdown">
             <button className="bg-gray-400 dropdown-btn">
-                <img src={session.user.picture} alt="User" className="m-auto w-14 h-14 object-cover rounded-full"></img>
+                <img src={session.user.picture} alt="User" className="m-auto w-14 h-14 object-cover rounded-full z-20"></img>
             </button>
                 <div className="border border-solid rounded-md dropdown-content">
-                    <p className="text-center cursor-default">------</p>
+                    <p className="pb-2 text-center font-extrabold cursor-default bg-transparent">---</p>
                     <a rel="noopener noreferrer" href="https://open.spotify.com/" target="_blank">
                         <p className="border-t border-solid border-gray-600 hover:bg-green-600 transition duration-300 ease-in-out dropdown-link">Spotify</p>
                     </a>
@@ -43,6 +43,8 @@ const UserProfile = () => {
                 <h1 className="p-1 text-5xl"><span className="font-bold text-green-500">f</span><span className="font-normal">m</span></h1>
             </div>
 
+
+            {/* navigation buttons */}
             {currentPath === '/browse' && (
                 <Link href='/dashboard'>
                     <button className="px-4 py-2 w-28 border border-solid border-white rounded-md hover:bg-green-600 transition duration-300 ease-in-out">Dashboard</button>
