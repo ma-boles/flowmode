8741833d682785f4602c0c81af1ee3b4491e45c3
ma-boles <ma_boles@hotmail.com>
2024-06-01 16:06:27 -0700

    git commit -m "removed signOut import"

--- my-nextjs-app/src/app/page.js
+++ my-nextjs-app/src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import './styles/styles.css';
 import Link from "next/link";
 
