84ce43a721e94b4f4345325a84ca11f63c5f6094
ma-boles <ma_boles@hotmail.com>
2024-06-04 15:50:55 -0700

    gitcommit -m "added transfer playback function"

--- my-nextjs-app/src/app/providers/PlayerProvider.js
+++ my-nextjs-app/src/app/providers/PlayerProvider.js
@@ -17,6 +17,16 @@ export const PlayerProvider = ({ children }) => {
     const [spotifyReady, setSpotifyReady] = useState(false);
     const [devices, setDevices] = useState([]);
 
+    const onDeviceIdChange = (newDeviceId) => {
+        setDeviceID(newDeviceId);
+    };
+
+    useEffect(() => {
+        if(deviceID) {
+            console.log('Device ID:', deviceID);
+        };
+    }, [deviceID]);
+
     const loadSDK = useCallback(() => {
         return new Promise((resolve, reject) => {
             const script = document.createElement("script");
@@ -58,11 +68,13 @@ export const PlayerProvider = ({ children }) => {
                 console.log('Spotify player is ready for playback.');
                 console.log('Device ID:', device_id);
                 setDeviceID(device_id);
-                // start playback or perform other actions here
+                transferPlayback(device_id, accessToken); // transfer playback to SDK
             });
     
             newPlayer.addListener('player_state_changed', (state) => {
-                console.log('Player state changed:', state);
+                if(state && state.device_id) {
+                    console.log('Player state changed:', state);
+                }
                 setPlayerState(state);
             });
     
@@ -89,12 +101,40 @@ export const PlayerProvider = ({ children }) => {
                 newPlayer.disconnect();
             };
     }, []);
-    
+
+    const transferPlayback = async (device_id, accessToken) => {
+        try {
+            const response = await fetch('https://api.spotify.com/v1/me/player', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify({
+                    device_ids: [device_id],
+                    play: true, //start playback
+                }),
+            });
+
+            if(response.status === 204) {
+                console.log('Playback transferred successfully');
+            } else {
+                console.error('Failed to transfer playback', response.status, response.statusText);
+            }
+        } catch(error) {
+            console.error('Error transferring playback:', error)
+        }
+    };
+
     useEffect(() => {
         // Define onSpotifyReady callback
         const onSpotifyReady = () => {
             console.log('Spotify Web Playback SDK is ready!');
-            initializePlayer(accessToken);
+            if(accessToken) {
+                initializePlayer(accessToken);
+            } else {
+                console.error('Access token is missing');
+            }
         };
     
         // Set onSpotifyWebPlaybackSDKReady callback
@@ -130,7 +170,16 @@ export const PlayerProvider = ({ children }) => {
                     throw new Error('Access token is missing');
                 }
                 const url = 'https://api.spotify.com/v1/me/player/devices';
-                const data = await makeApiRequest(url, accessToken);
+                const response = await fetch(url, {
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                });
+                if(!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                /*const data = await makeApiRequest(url, accessToken);*/
                 setDevices(data.devices);
             } catch (error) {
                 console.error('Error fetching devices:', error.message);
@@ -141,6 +190,7 @@ export const PlayerProvider = ({ children }) => {
     }, [accessToken]);
 
     const playItem = useCallback((uri) => {
+        console.log('Attempting to play item with URI:', uri);
         console.log(player);
 
         if (player) {
@@ -156,147 +206,7 @@ export const PlayerProvider = ({ children }) => {
         }
        }, [player]);
 
-/*
-    const onSpotifyReady = useCallback(() => {
-        console.log('Spotify Web Playback SDK is ready!');
-        initializePlayer(accessToken);
-    }, [accessToken]);
 
-   useEffect(() => {
-    // Define onSpotifyReady callback
-    const onSpotifyReady = () => {
-        console.log('Spotify Web Playback SDK is ready!');
-        initializePlayer(accessToken);
-    };
-
-    // Set onSpotifyWebPlaybackSDKReady callback
-    window.onSpotifyWebPlaybackSDKReady = onSpotifyReady;
-
-    // Clean up event listener when component unmounts
-    return () => {
-        delete window.onSpotifyWebPlaybackSDKReady;
-    };
-   }, [/*accessToken onSpotifyReady]);
-
-
-   useEffect(() => {
-    const loadSDKScript = async () => {
-        try {
-            await loadSDK();
-            setSpotifyReady(true);
-        } catch (error) {
-            console.error('Failed to load Spotify SDK:', error);
-        }
-    };
-
-    loadSDKScript();
-
-    return () => {
-        removeSDKScript();
-    };
-   }, []);
-
-   useEffect(() => {
-        if (accessToken && spotifyReady) {
-            initializePlayer(accessToken);
-        }
-   }, [accessToken, spotifyReady]);
-
-   const loadSDK = useCallback(() => {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement("script");
-        script.src = "https://sdk.scdn.co/spotify-player.js";
-        script.async = true;
-        script.onload = resolve;
-        script.onerror = reject;
-        document.body.appendChild(script);
-    });
-   }, []);
-
-   const removeSDKScript = useCallback(() => {
-    const script = document.querySelector('script[src="https://sdk.scdn.co/spotify-player.js"]');
-    if(script) {
-        script.remove();
-    }
-   }, []);
-
-   const initializePlayer = useCallback((accessToken) => {
-    try {
-        const newPlayer = new window.Spotify.Player({
-            name: 'Flow Mode Player',
-            getOAuthToken: (cb) => cb(accessToken),
-            volume: 0.5
-        });
-
-        setPlayer(newPlayer);
-
-        // Ready
-        newPlayer.addListener('ready', ({ device_id }) => {
-            console.log('Spotify player is ready for playback.');
-            console.log('Device ID:', device_id);
-            setDeviceID(device_id);
-            // start playback or perform other actions here
-        });
-
-        newPlayer.addListener('player_state_changed', (state) => {
-            console.log('Player state changed:', state);
-            setPlayerState(state);
-        });
-
-        // Not ready
-        newPlayer.addListener('not_ready', ({ device_id }) => {
-            console.log('Device ID has gone offline', device_id)
-        });
-
-        newPlayer.addListener('initialization_error', ({ message }) => {
-            console.log('Initializtion error:', message);
-        });
-
-        newPlayer.addListener('authentication_error', ({ message}) => {
-            console.log('Authentication error:', message);
-        });
-
-        newPlayer.addListener('account_error', ({ message}) => {
-            console.log('Account error:', message);
-        });
-
-        newPlayer.connect();
-    } catch (error) {
-        console.error('Error initializing Spotify player:', error)
-    }
-   }, []);
-
-   useEffect(() => {
-    const fetchDevices = async () => {
-        try {
-            if(!accessToken) {
-                throw new Error('Access token is missing');
-            }
-            const url = 'https://api.spotify.com/v1/me/player/devices';
-            const data = await makeApiRequest(url, accessToken);
-            setDevices(data.devices);
-        } catch (error) {
-            console.error('Error fetching devices:', error.message);
-        }
-    };
-
-    fetchDevices();
-   }, [accessToken]);
-
-   const playItem = useCallback((uri) => {
-    console.log(player);
-    if (player) {
-        player.togglePlay({ uris: [uri]})
-        .then(() => {
-            console.log('Item playback started');
-        })
-        .catch((error) => {
-            console.error('Error playing item:', error);
-        });
-    } else {
-        console.error('Player is not available.');
-    }
-   }, [player]);*/
 
     // Bundle up the context value with state variables and functions
     const contextValue = React.useMemo(() => ({
@@ -306,7 +216,10 @@ export const PlayerProvider = ({ children }) => {
         spotifyReady,
         initializePlayer,
         playItem,
-    }), [player, deviceID, playerState, spotifyReady, initializePlayer, playItem]);
+        setPlayerState,
+        onDeviceIdChange
+    }), [player, deviceID, playerState, spotifyReady, initializePlayer, playItem, setPlayerState, onDeviceIdChange
+    ]);
 
 
     return (
