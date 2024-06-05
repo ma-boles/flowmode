149ff5951c574e8dd45fe22ca265a0e015b0c73d
ma-boles <ma_boles@hotmail.com>
2024-06-05 14:46:11 -0700

    git commit -m "conditional render minutes onClick"

--- my-nextjs-app/src/components/userdata/TotalMinutes.js
+++ my-nextjs-app/src/components/userdata/TotalMinutes.js
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TotalMinutes () {
+    const [minutesCard, setMinutesCard] = useState('today');
 
-    const click = () => {
-        alert('click')
+    const showMinutesDay = () => {
+        setMinutesCard('today')
+    };
+
+    const showMinutesWeek = () => {
+        setMinutesCard('week')
+    };
+
+    const showMinutesMonth = () => {
+        setMinutesCard('month')
     };
 
     return (
         <>
-            <div className="border-8 border-solid border-white rounded-lg opacity-50">
-                <div className="px-2 flex w-full justify-between">
-                    <button className="mt-2 w-24 border border-sold border-blue-300 hover:bg-blue-600" onClick={click}>Today</button>
-                    <button className="mt-2 w-24 border border-solid border-blue-300  hover:bg-blue-600">This Week</button>
-                    <button className="mt-2 w-24 border border-solid border-blue-300  hover:bg-blue-600">This Month</button>
+            <div className="border-8 border-solid border-gray-100 rounded-lg border-opacity-80">
+
+                <div /* buttons div */ className="px-2 flex w-full justify-between">
+                    <button className={`mt-2 w-24 border border-solid border-blue-300 hover:bg-blue-600 ${minutesCard === 'today' ? 'bg-blue-600'  : 'bg-transparent'}`}
+                    onClick={showMinutesDay}
+                    >Today</button>
+                    <button className={`mt-2 w-24 border border-solid border-blue-300  hover:bg-blue-600 
+                    ${minutesCard === 'week' ? 'bg-blue-600' : 'bg-transparent'}`} 
+                    onClick={showMinutesWeek}
+                    >This Week</button>
+                    <button className={`mt-2 w-24 border border-solid border-blue-300  hover:bg-blue-600 
+                    ${minutesCard === 'month' ? 'bg-blue-600' : 'bg-transparent'}`} 
+                    onClick={showMinutesMonth}
+                    >This Month</button>
                 </div>
-                <div className="px-8 py-6 mb-0 mx-8 ">
-                    <h2 className="m-2 font-bold text-2xl opacity-90">Minutes of <br /> Focused Work:</h2>
+
+            {minutesCard === 'today' &&
+            <>
+                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
+                    <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
                     <hr />
-                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">00</span> MIN</h2>
+                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">10</span> MIN</h2>
                 </div>
+            </>
+            }
+
+            {minutesCard === 'week' &&
+            <>
+                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
+                    <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
+                    <hr />
+                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">20</span> MIN</h2>
+                </div>
+            </>
+            }
+
+            {minutesCard === 'month' &&
+            <>
+                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
+                    <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
+                    <hr />
+                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">30</span> MIN</h2>
+                </div>
+            </>
+            }
+
             </div>
         </>
     )
