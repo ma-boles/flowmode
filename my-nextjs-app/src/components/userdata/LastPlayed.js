ef682ef427da2fc2c6fdded293d08ad27a004990
ma-boles <ma_boles@hotmail.com>
2024-06-05 14:45:36 -0700

    git commit -m "updated stylings"

--- my-nextjs-app/src/components/userdata/LastPlayed.js
+++ my-nextjs-app/src/components/userdata/LastPlayed.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function LastPlayed() {
     return(
         <>
-            <div className="py-6 px-12 mx-8 border-8 border-solid border-white rounded-lg opacity-50">
+            <div className="py-6 px-12 mx-8 border-8 border-solid border-gray-100 rounded-lg border-opacity-80">
                 <h2 className="m-2 font-bold text-2xl opacity-90">Most Recently <br /> Played:</h2>
                 <hr />
-                <div className="flex flex-col">
+                <div className="flex flex-col opacity-90">
                     <h2 className="m-2 text-xl font-semibold">Title #1</h2>
                     <h2 className="m-2 text-xl font-semibold">Title #2</h2>
                     <h2 className="m-2 text-xl font-semibold">Title #3</h2>
