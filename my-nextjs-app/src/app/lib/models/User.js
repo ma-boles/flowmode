092e29f5e163a771b0a482a8d5883424ce3cb760
ma-boles <ma_boles@hotmail.com>
2024-06-14 15:46:45 -0700

    git commit -m "added user schema"

---
+++ my-nextjs-app/src/app/lib/models/User.js
@@ -0,0 +1,40 @@
+// mongoose
+
+
+const userSchema = new mongoose.Schema({
+    spotifyId: {
+        type: String,
+        required: true,
+    },
+    spotifyName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    timePerDay: {
+        type: Number,
+        default: 0,
+    },
+    timePerWeek: {
+        type: Number,
+        default: 0,
+    },
+    timePerMonth: {
+        type: Number,
+        default: 0,
+    },
+});
+
+let User;
+try {
+    User = mongoose.model('User');
+} catch(error) {
+    User = mongoose.model('User', userSchema);
+}
+
+
+export default User;
