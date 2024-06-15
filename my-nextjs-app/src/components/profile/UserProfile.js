fd19c4f040cfd4488e3bfb6248799fc0c747d947
ma-boles <ma_boles@hotmail.com>
2024-06-14 17:48:08 -0700

    git commit -m "changed opt-in link color"

--- my-nextjs-app/src/components/profile/UserProfile.js
+++ my-nextjs-app/src/components/profile/UserProfile.js
@@ -41,7 +41,7 @@ const UserProfile = () => {
                     <Link href='/how'>
                         <p className="border-t border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">How To</p>
                     </Link>
-                    <button className="w-full border-t border-solid border-gray-600 hover:bg-purple-600 transition duration-300 ease-in-out dropdown-link" onClick={() => setIsOpen(true)}>Opt-In</button>
+                    <button className="w-full border-t border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link" onClick={() => setIsOpen(true)}>Opt-In</button>
                     <a rel="noopener noreferrer" href="https://open.spotify.com/" target="_blank">
                         <p className="border-t border-solid border-gray-600 hover:bg-green-600 transition duration-300 ease-in-out dropdown-link">Spotify</p>
                     </a>
