5ef2d3bddf90f33c41fb7c401b04d00b1c3e7cc9
ma-boles <ma_boles@hotmail.com>
2024-06-21 16:36:47 -0700

    git commit -m "updated player controls"

--- my-nextjs-app/src/components/player/Player.js
+++ my-nextjs-app/src/components/player/Player.js
@@ -8,72 +8,31 @@ import "@/app/styles/styles.css"
 
 
 export default function Player() {
-    const { player, playerState } = usePlayer();
+    const { player, playerState, play, pause, next, previous } = usePlayer();
     // Check if the player is currently playing something
-   // const isPlaying = playerState && playerState.isPlaying;
+    //const isPlaying = playerState && playerState.isPlaying;
     const [isFlowVisible, setIsFlowVisible] = useState(false);
-    const [activeInterval, setActiveInterval] = useState('flow');
-    const [intervalChange, setIntervalChange] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
 
-    const handleIntervalChange = (interval) => {
-        setIntervalChange(interval);
-    };
-
-    console.log('activeInterval:', activeInterval); // Log to check state changes
-
-    const handlePlayPause = () => {
-        try {
-            if (player && player.togglePlay) {
-                console.log('Toggle play initiated');
-                player.togglePlay();
-            } else {
-                console.log('Player or togglePlay method not available.');
-            }
-        } catch (error) {
-            console.error('Error occurred while toggling play/pause:', error);
-        }
-    };
-
-    const handleSkip = () => {
-        try {
-            if (player) {
-                player.nextTrack();
-            } else {
-                console.error('Player not available.');
-            }
-        } catch (error) {
-            console.error('Error occurred while skipping track:', error);
-        }
-    };
-
-    const handleBack = () => {
-        try {
-            if (player) {
-                player.previousTrack();
-            } else {
-                console.error('Player not available.');
-            }
-        } catch (error) {
-            console.error('Error occurred while going back to previous track:', error);
-        }
-    };
+    if(!player || !playerState) {
+        return <div>Loading...</div>;
+    }
 
     const handleFlow = () => {
         setIsFlowVisible(!isFlowVisible);
     };
 
     //console.log('Player:', player);
-    console.log('Player state:', playerState);
+    //console.log('Player state:', playerState);
 
 
     return (
         <div className="player">
             <div className="playBackground" /*justify-center*/>
                 {isFlowVisible &&
-                    <div /* flow div */ className={`mx-32 mt-16 mb-10 border border-solid border-gray-800 rounded-md ${activeInterval === 'refresh' ? 'bg-blue-500' : 'bg-transparent'}`}>
-                    <FlowTimer intervalChange={intervalChange} setIntervalChange={handleIntervalChange} />
-            </div>
+                    <div /* flow div */ >
+                        <FlowTimer />
+                    </div>
             }
                 <div className="flex justify-between">
 
@@ -83,11 +42,11 @@ export default function Player() {
 
                     <div className="flex px-32 justify-between bg-transparent">
 
-                        <button className="playerBtnSm" onClick={handleBack}>
+                        <button className="playerBtnSm" onClick={previous}>
                             <img src="backward-step-solid.svg" alt="back" className="btnIconSm"></img>
                         </button>
 
-                        <button className="playerBtn" onClick={handlePlayPause}>
+                        <button className="playerBtn" onClick={play}>
                             {playerState === 'playing' ? (
                                 <img src="/pause-solid.svg" alt="pause" className="btnIcon"></img>
                             ) : (
@@ -95,7 +54,7 @@ export default function Player() {
                             )}
                         </button>
 
-                        <button className="playerBtnSm" onClick={handleSkip}>
+                        <button className="playerBtnSm" onClick={next}>
                             <img src="forward-step-solid.svg" alt="skip" className="btnIconSm"></img>
                         </button>
                     </div>
@@ -123,58 +82,3 @@ export default function Player() {
         </div>
     )
 }
-                /*<div /* flow div  className="pt-18 pb-12 mb-8 mt-8">*/
-
-
-                    {/*<h2 className="pt-16 pb-8 text-4xl"><span className="font-bold text-green-600">flow</span> Settings</h2>
-                    <div className="w-4/5 flex justify-center border border-solid border-white rounded-t-lg mx-auto flowDiv">
-                        <div className="w-1/3 my-4 flex flex-col justify-center items-center" /* work *//*>
-                            <h2 className="text-xl font-bold">Work:</h2>
-                            <div className="flex justify-center items-center">
-                                <input className="inputTimer"
-                                type="number"
-                                placeholder="00"
-                                min="0"
-                                max="10"
-                                />
-                            <p>HR</p>
-                                <input className="inputTimer"
-                                type="number"
-                                placeholder="00"
-                                min="0"
-                                max="59"
-                                />
-                            <p>MIN</p>
-                            </div>
-                        </div>
-
-                        <div className="w-1/3 flex flex-col justify-center items-center" /* rest *//*>
-                            <h2 className="text-xl font-bold">Rest:</h2>
-                            <div className="flex justify-center items-center">
-                                <input className="inputTimer"
-                                type="number"
-                                placeholder="00"
-                                min="1"
-                                max="59"
-                                />
-                            <p>MIN</p>
-                            </div>
-                        </div>
-
-                        <div className="w-1/3 flex justify-center items-center" /* intervals *//*>
-                            <div className="flex justify-center items-center">
-                            <h2 className="text-xl">Number of <br /> Intervals:</h2>
-                                <input className="inputTimer"
-                                type="number"
-                                placeholder="1"
-                                min="1"
-                                max="10"/>
-                            </div>
-                        </div>
-                </div>
-
-
-                <div className="w-4/5 m-auto mb-4 p-4 flex justify-end items-center border border-solid border-white rounded-b-lg flowDiv2"/* buttons *//*>
-                    <button className="mx-4 py-1 px-6 text-gray font-bold bg-blue-500 border border-solid border-blue-300 hover:bg-blue-700">RESET</button>
-                    <button className="mx-4 py-1 px-8 text-gray font-bold bg-blue-500 border border-solid border-blue-300 hover:bg-blue-700">SET</button>
-                </div>*/}
