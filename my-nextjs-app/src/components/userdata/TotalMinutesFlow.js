e2cb82619b60e8f17206dd10348cf002cf2d2b21
ma-boles <ma_boles@hotmail.com>
2024-06-25 15:30:01 -0700

    git commit -m "updated card styles"

--- my-nextjs-app/src/components/userdata/TotalMinutesFlow.js
+++ my-nextjs-app/src/components/userdata/TotalMinutesFlow.js
@@ -35,30 +35,30 @@ export default function TotalMinutesFlow () {
 
             {minutesCard === 'today' &&
             <>
-                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
+                <div className="px-8 py-3 mb-0 mx-8 opacity-90">
                     <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
                     <hr />
-                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">10</span> MIN</h2>
+                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">07 </span>HR <br /> <span className="text-4xl">10</span> MIN</h2>
                 </div>
             </>
             }
 
             {minutesCard === 'week' &&
             <>
-                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
+                <div className="px-8 py-3 mb-0 mx-8 opacity-90">
                     <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
                     <hr />
-                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">20</span> MIN</h2>
+                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">15 </span>HR <br /> <span className="text-4xl">20</span> MIN</h2>
                 </div>
             </>
             }
 
             {minutesCard === 'month' &&
             <>
-                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
+                <div className="px-8 py-3 mb-0 mx-8 opacity-90">
                     <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
                     <hr />
-                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">30</span> MIN</h2>
+                    <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">30 </span>HR <br /> <span className="text-4xl">30</span> MIN</h2>
                 </div>
             </>
             }
--- my-nextjs-app/src/components/userdata/TotalMinutesRest.js
+++ my-nextjs-app/src/components/userdata/TotalMinutesRest.js
@@ -20,7 +20,7 @@ export default function TotalMinutesRest () {
             <div className="border-8 border-solid border-gray-100 rounded-lg border-opacity-80">
 
                 <div /* buttons div */ className="px-2 flex w-full justify-between">
-                    <button className={`mt-2 w-24 border border-solid border-blue-300 hover:bg-blue-600 ${minutesCard === 'today' ? 'bg-blue-600'  : 'bg-transparent'}`}
+                    <button className={`mt-2 w-24 border border-solid border-blue-300 hover:bg-blue-600 ${restCard === 'today' ? 'bg-blue-600'  : 'bg-transparent'}`}
                     onClick={showRestDay}
                     >Today</button>
                     <button className={`mt-2 w-24 border border-solid border-blue-300  hover:bg-blue-600 
@@ -35,8 +35,8 @@ export default function TotalMinutesRest () {
 
             {restCard === 'today' &&
             <>
-                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
-                    <h2 className="m-2 font-bold text-2xl">Total<br />Minutes of <br /> Rest:</h2>
+                <div className="px-8 py-3 mb-0 mx-8 opacity-90">
+                    <h2 className="m-2 font-bold text-2xl">Total Minutes <br />of Rest:</h2>
                     <hr />
                     <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">10</span> MIN</h2>
                 </div>
@@ -45,8 +45,8 @@ export default function TotalMinutesRest () {
 
             {restCard === 'week' &&
             <>
-                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
-                    <h2 className="m-2 font-bold text-2xl">Total <br />Minutes of <br /> Rest:</h2>
+                <div className="px-8 py-3 mb-0 mx-8 opacity-90">
+                    <h2 className="m-2 font-bold text-2xl">Total Minutes <br />of Rest:</h2>
                     <hr />
                     <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">20</span> MIN</h2>
                 </div>
@@ -55,8 +55,8 @@ export default function TotalMinutesRest () {
 
             {restCard === 'month' &&
             <>
-                <div className="px-8 py-6 mb-0 mx-8 opacity-90">
-                    <h2 className="m-2 font-bold text-2xl">Total <br />Minutes of <br /> Rest:</h2>
+                <div className="px-8 py-3 mb-0 mx-8 opacity-90">
+                    <h2 className="m-2 font-bold text-2xl">Total Minutes<br />of Rest:</h2>
                     <hr />
                     <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">00 </span>HR <br /> <span className="text-4xl">30</span> MIN</h2>
                 </div>
