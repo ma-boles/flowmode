1efb286891224bc646d42e274a1e1046e0b4e71d
ma-boles <ma_boles@hotmail.com>
2024-06-27 14:32:08 -0700

    git commit -m "updated heading styles"

--- my-nextjs-app/src/app/dashboard/page.js
+++ my-nextjs-app/src/app/dashboard/page.js
@@ -63,15 +63,15 @@ export default function Dashboard() {
                 <NavBar />
             </nav>
 
-            <div className="m-12">
+            <div className="mx-12 my-8">
                 {!session ? (
                         <h1 className="text-center">Loading...</h1>
                 ): (
-                    <h1 className="p-0 mb-14 text-center">Welcome {session.user.name}!</h1>
+                    <h1 className="p-0 mb-12 text-5xl text-center">Welcome {session.user.name}!</h1>
 
                 )}
 
-                    <div /* top section */ className="mb-16 flex justify-center">
+                    <div /* top section */ className="mb-16 mt-8 flex justify-center">
                     
 
                         <div /* minute totals div */>
