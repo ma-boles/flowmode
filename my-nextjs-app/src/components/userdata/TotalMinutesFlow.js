6d1de7bc97f28195df884b29b240511ec75d8a2b
ma-boles <ma_boles@hotmail.com>
2024-06-27 14:33:14 -0700

    git commit -m "updated styles"

--- my-nextjs-app/src/components/userdata/TotalMinutesFlow.js
+++ my-nextjs-app/src/components/userdata/TotalMinutesFlow.js
@@ -19,7 +19,7 @@ export default function TotalMinutesFlow () {
         <>
             <div className="border-8 border-solid border-gray-100 rounded-lg border-opacity-80">
 
-                <div /* buttons div */ className="px-2 flex w-full justify-between">
+                <div /* buttons div */ className="flex w-full justify-center">
                     <button className={`mt-2 w-24 border border-solid border-blue-300 hover:bg-blue-600 ${minutesCard === 'today' ? 'bg-blue-600'  : 'bg-transparent'}`}
                     onClick={showMinutesDay}
                     >Today</button>
@@ -36,7 +36,7 @@ export default function TotalMinutesFlow () {
             {minutesCard === 'today' &&
             <>
                 <div className="px-8 py-3 mb-0 mx-8 opacity-90">
-                    <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
+                    <h2 className="mx-2 mt-1 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
                     <hr />
                     <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">07 </span>HR <br /> <span className="text-4xl">10</span> MIN</h2>
                 </div>
@@ -46,7 +46,7 @@ export default function TotalMinutesFlow () {
             {minutesCard === 'week' &&
             <>
                 <div className="px-8 py-3 mb-0 mx-8 opacity-90">
-                    <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
+                    <h2 className="mx-2 mt-1 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
                     <hr />
                     <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">15 </span>HR <br /> <span className="text-4xl">20</span> MIN</h2>
                 </div>
@@ -56,7 +56,7 @@ export default function TotalMinutesFlow () {
             {minutesCard === 'month' &&
             <>
                 <div className="px-8 py-3 mb-0 mx-8 opacity-90">
-                    <h2 className="m-2 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
+                    <h2 className="mx-2 mt-1 font-bold text-2xl">Minutes of <br /> Focused Work:</h2>
                     <hr />
                     <h2 className="mx-2 mt-6 mb-2 font-bold text-2xl opacity-90"><span className="text-4xl">30 </span>HR <br /> <span className="text-4xl">30</span> MIN</h2>
                 </div>
