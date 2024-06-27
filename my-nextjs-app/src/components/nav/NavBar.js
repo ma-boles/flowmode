505bb7fa6b72fac9a2a81e80ed6e5c6f99e16ca2
ma-boles <ma_boles@hotmail.com>
2024-06-27 13:25:42 -0700

    git commit -m "renamed files"

--- my-nextjs-app/src/components/nav/NavBar.js
+++ my-nextjs-app/src/components/nav/NavBar.js
@@ -1,37 +1,111 @@
-import React from 'react';
-import Link from 'next/link';
-import '@/app/styles/styles.css'
+import React, { useEffect, useState } from "react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import Modal from "../Modal";
 
-export default function NavBar({ accessToken, scrollToRef }) {
 
-    /*const accessToken = accessToken;*/
+const NavBar = () => {
+    const { data: session } = useSession();
+    const [isClient, setIsClient] = useState(false);
+    const [currentPath, setCurrentPath] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
 
-    function changeText() {
-        var timeRemaining = document.getElementById('time');
-        timeRemaining.textContent = '00:00';
+    useEffect(() => {
+        setIsClient(true);
+        if(typeof window !== 'undefined') {
+            setCurrentPath(window.location.pathname);
+        }
+    }, []);
+
+    if(!isClient) {
+        return null;// Loading state
+    };
+
+    if (!session) {
+        return <p>Please log in</p>;
     }
 
+
     return(
-        <div>
-            <section className='flex justify-between my-0 mx-4 w-full'>
-                <div className='mt-4 ml-4'>
-                    <p className='mt-2 ml-4 font-bold text-base'>Spotify Reads</p>
-                    {/*<UserProfile accessToken={accessToken}/>*/}
+        <div className="flex px-8 justify-between w-full items-center navbar">
+            <div className="dropdown">
+            <button className="p-0.5 my-2 bg-gray-400 dropdown-btn">
+                {session.user.image ? (
+                <img src={session.user.image} alt="User" className="m-auto w-14 h-14 object-cover rounded-full"></img>
+                ) : (
+                    <img src="user-regular.svg" alt="User" className="m-auto w-14 h-14 object-cover"></img>
+                )}
+            </button>
+                <div className="border border-solid rounded-sm dropdown-content">
+                    <Link href='/get-started'>
+                        <p className="hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">Get Started</p>
+                    </Link>
+                    <button className="w-full text-left border-t border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link" onClick={() => setIsOpen(true)}>Account</button>
+                    <Link href='/about'>
+                        <p className="border-t border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">About</p>
+                    </Link>
+                    <a rel="noopener noreferrer" href="https://open.spotify.com/" target="_blank">
+                        <p className="border-t border-b border-solid border-gray-600 hover:bg-green-600 transition duration-300 ease-in-out dropdown-link">Spotify</p>
+                    </a>
+                    <button onClick={() => signOut('spotify')} className="w-full text-left hover:bg-red-600 transition duration-300 ease-in-out dropdown-link">Log Out</button>
                 </div>
-                <div className='links--section'>
-                    <ul className='mr-8 list-none'>
-                        <li onClick={() => scrollToRef('Bookshelf')}>Bookshelf</li>
-                        <li onClick={() => scrollToRef('Queue')}>Queue</li>
-                        <li onClick={() => scrollToRef('ReadingList')}>Reading List</li>
+            </div>
 
-                        <Link href="/browse"><li>Browse</li></Link>
-                        <Link href="/dashboard"><li>Dashboard</li></Link>
+            {/* Opt In modal */}
+            <div className="centered">
+            {isOpen && <Modal setIsOpen={setIsOpen} />}
+            </div>
 
-                        <li className='p-1 text-center text-base bg-transparent w-20 border-solid border-2 border-neutral-100 cursor-pointer' id='time' onClick={changeText}>
-                            <p className=' text-sm'>Time Left</p></li>
-                    </ul>
+            <div /* center logo */>
+                <button className=" hover:bg-gray-600">
+                    <h1 className="p-1 text-5xl"><span className="font-bold text-green-500">f</span><span className="font-normal">m</span></h1>
+                </button>
+            </div>
+
+
+            {/* navigation buttons */}
+            {currentPath === '/search' && (
+                <Link href='/dashboard'>
+                    <button className="px-4 py-2 w-28 border border-solid border-white rounded-md hover:bg-green-600 transition duration-300 ease-in-out">Dashboard</button>
+                </Link>
+            )}
+
+            {currentPath === '/dashboard' && (
+                <Link href='/search'>
+                    <button className="px-4 py-2 w-28 border border-solid border-white rounded-md hover:bg-green-600 transition duration-300 ease-in-out">Search</button>
+                </Link>
+            )}
+
+            {currentPath === '/about' && (
+                <div className="text-center rounded-md dropdown-nav">
+                    <button className="border border-solid border-green-600 font-semibold dropdown-nav-btn">Nav</button>
+                    <div className="border border-solid border-green-600 dropdown-nav-content">
+                        <Link href='/search'>
+                            <p className="p-2 border-b border-solid border-gray-400 font-semibold  hover:bg-green-600 transition duration-300 ease-in-out">Search</p>
+                        </Link>
+                        <a href="/dashboard">
+                            <p className="p-2 border-t border-solid border-gray-400 font-semibold hover:bg-green-600 transition duration-300 ease-in-out">Dashboard</p>
+                        </a>
+                    </div>
                 </div>
-            </section>
+            )}
+
+            {currentPath === '/get-started' && (
+                <div className="text-center rounded-md dropdown-nav">
+                    <button className="border border-solid border-green-600 font-semibold dropdown-nav-btn">Nav</button>
+                    <div className="border border-solid border-green-600 dropdown-nav-content">
+                        <Link href='/search'>
+                            <p className="p-2 border-b border-solid border-gray-400 font-semibold  hover:bg-green-600 transition duration-300 ease-in-out">Search</p>
+                        </Link>
+                        <a href="/dashboard">
+                            <p className="p-2 border-t border-solid border-gray-400 font-semibold hover:bg-green-600 transition duration-300 ease-in-out">Dashboard</p>
+                        </a>
+                    </div>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
+
+
+export default NavBar;
