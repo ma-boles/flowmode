906e4661491020e1feedf696de89749dd4b98771
ma-boles <ma_boles@hotmail.com>
2024-07-02 15:24:26 -0700

    git commit -m "removed conditional rendering"

--- my-nextjs-app/src/components/ItemCardButton.js
+++ my-nextjs-app/src/components/ItemCardButton.js
@@ -1,23 +1,15 @@
 import React from "react";
-import { usePlaylistContext } from "@/app/contexts/PlaylistContext";
 
-export default function ItemCardButton ({ category, playlist, onSelectFlow, onSelectPreview, onSelectRest }) {
-    const { onSelectFlow, onSelectRest, onSelectPreview } = usePlaylistContext();
+export default function ItemCardButton ({ playlist, onSelectFlow, onSelectPreview, onSelectRest }) {
 
     return(
             <div className="ellipsis--grid--div">
                 <button className={`text-2xl font-bold ellipsis--grid--button`}>&#8230;</button>
                 <div className="ellipsis--grid--content text-center">
                     <ul>
-                        {(category === 'playlist' || 'episode' || 'audiobook') && (
-                            <>
-                                <li className="py-1 font-semibold border-b border-solid border-gray-500 hover:bg-blue-500" onClick={() => onSelectFlow(playlist.id)}>Flow</li>
-                                <li className="py-1 font-semibold border-b border-solid border-gray-500 hover:bg-blue-500" onClick={() => onSelectRest(playlist.id)}>Rest</li>
-                            </>
-                         )}
-                        {(category === 'song' || 'album' || 'audiobook' || 'episode') && (
-                            <li className="py-1 font-semibold hover:bg-blue-500" onClick={() => onSelectPreview(playlist.id)}>Preview</li>
-                        )}
+                        <li className="py-1 font-semibold border-b border-solid border-gray-500 hover:bg-blue-500" onClick={() => onSelectFlow(playlist.id)}>Flow</li>
+                        <li className="py-1 font-semibold border-b border-solid border-gray-500 hover:bg-blue-500" onClick={() => onSelectRest(playlist.id)}>Rest</li>
+                        <li className="py-1 font-semibold hover:bg-blue-500" onClick={() => onSelectPreview(playlist.id)}>Preview</li>
                     </ul>
                 </div>
             </div>
