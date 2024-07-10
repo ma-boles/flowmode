9f0b693c268885d6a1029624c75b2d2cf82cd03e
ma-boles <ma_boles@hotmail.com>
2024-07-09 17:30:03 -0700

    git commit -m "changed styles for both sections"

--- my-nextjs-app/src/app/about/page.js
+++ my-nextjs-app/src/app/about/page.js
@@ -10,13 +10,14 @@ export default function About () {
             <NavBar />
         </div>
 
-        <div className="min-h-screen">
-            <div /* mission */ className="m-24">
+        <div className="min-h-screen mx-24">
+
+            <div /* mission */ className="my-24 px-12 pt-4 pb-8 rounded-lg about--content">
                 <h2 className="my-4 text-3xl font-bold">Our Mission</h2>
                 <p className="text-xl">At <span className="text-green-600 font-bold">flow</span><span className="font-medium">mode</span>, our mission is to enhance productivity and well-being for those who work long hours. By integrating customizable work timers with Spotify audio, we empower users to stay focused, motivated, and refreshed, fostering a balanced and productive work environment.</p>
             </div>
             
-            <div /* story */ className="m-24">
+            <div /* story */ className="my-24 px-12 pt-4 pb-8 rounded-lg about--content">
                 <h2 className="my-4 text-3xl font-bold">Our Story</h2>
                 <p className="text-xl">The idea for <span className="text-green-600 font-bold">flow</span><span className="font-medium">mode</span> originated from my own struggle to mintain focus and productivty during long work hours ofen leading to fatigue and headaches. In an effort to improve my situation, I sought out information pertaining to time management and productivity and discovered the importance of breaks and the Pomodoro technique. After much trial and error, I realized the need for a flexibile tool that would accomaodate different work environments based on user preferences.<br /><br />
                 As a result <span className="text-green-600 font-bold">flow</span><span className="font-medium">mode</span> was developed. It empowers users to customize their environments regaradless of their listening preferences or desired work/rest intervals. <br /><br />
