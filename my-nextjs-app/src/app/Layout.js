7e9c008ad43194c42e95fb279bcf8192f3f61a17
ma-boles <ma_boles@hotmail.com>
2024-07-09 17:27:53 -0700

    git commit -m "added PlayerProvider"

--- my-nextjs-app/src/app/Layout.js
+++ my-nextjs-app/src/app/Layout.js
@@ -1,10 +1,14 @@
+'use client'
 import React from 'react';
 import HeadComponent from '../components/HeadComponent';
 import Footer from '../components/footer/Footer';
+//import Player from '../components/player/Player';
 import '@/app/styles/globals.css';
 import '@/app/styles/styles.css';
-import { ShelfProvider } from './contexts/ShelfContext';
+//import { ShelfProvider } from '../../../spotify-reads/src/components/ShelfContext';
 import PageWrapper from '@/components/PageWrapper';
+import { PlaylistProvider } from './contexts/PlaylistContext';
+import { PlayerProvider } from './providers/PlayerProvider';
 
 
 export default function RootLayout ({ metadata, children, session }) {
@@ -13,14 +17,16 @@ export default function RootLayout ({ metadata, children, session }) {
     <>
       <html lang="en">
         <HeadComponent metadata={metadata}/>
-        <body>
-              <ShelfProvider>
-                <PageWrapper session={session}>
+          <body>
+            <PlaylistProvider>
+              <PageWrapper session={session}>
+                <PlayerProvider>
                   {children}
-                </PageWrapper>
-              </ShelfProvider>
-          <Footer />
-        </body>
+                </PlayerProvider>
+              </PageWrapper>
+            </PlaylistProvider>
+            <Footer />
+          </body>
       </html>
     </>
   );
