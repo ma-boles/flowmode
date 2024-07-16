837f1d34c274ac9c359e94c7052ef9a4d8155f78
ma-boles <ma_boles@hotmail.com>
2024-07-15 18:07:21 -0700

    git commit -m "updated conditional styles"

--- my-nextjs-app/src/components/player/Player.js
+++ my-nextjs-app/src/components/player/Player.js
@@ -1,27 +1,25 @@
 'use client'
 import React, { createContext, useContext, useState } from "react";
 import { PlayerContext } from "@/app/providers/PlayerProvider";
+import { useSession } from "next-auth/react";
 import usePlayer from "@/app/hooks/usePlayer";
 import { skipTrack, previousTrack, togglePlay } from "@/app/lib/playerApi";
 import { TrackInfo } from "./TrackInfo";
 import FlowTimer from "./FlowTimer";
 import "@/app/styles/styles.css"
-import { useSession } from "next-auth/react";
 
 
 export default function Player() {
-    const { accessToken } = useSession();
+    const { data: session } = useSession();
+    const accessToken = session?.accessToken;
     const { player, playerState } = usePlayer();
 
-    // Check if the player is currently playing something
-    //const isPlaying = playerState && playerState.isPlaying;
+    const { paused, context } = playerState || {};
+
     const [isFlowVisible, setIsFlowVisible] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
 
-    if(!player || !playerState) {
-        return <h2 className="mb-4 text-center text-2xl font-semibold">Player loading...</h2>;
-    }
-
+    // open flow component
     const handleFlow = () => {
         setIsFlowVisible(!isFlowVisible);
     };
@@ -29,6 +27,14 @@ export default function Player() {
 
     // player controls
     const handleTogglePlay = async() => {
+        console.log('Access Token:', accessToken);
+        console.log('Player State:', playerState);
+
+        if(!player) {
+            console.error('Spotify player is not initialized');
+            return;
+        }
+        
         if(accessToken) {
             await togglePlay(accessToken, playerState);
         } else {
@@ -53,18 +59,17 @@ export default function Player() {
     };
 
 
+
+    // conditional styling
+
     // extract playing status from playerState
-    const { device } = playerState;
-    const isPlaying = device.is_active;
+    const isPlaying = !paused;
+    const isPlaylist = context?.uri?.startsWith('spotify:playlist:');
+
+    const isPlayingPlaylist = isPlaying && isPlaylist;
 
     // determine is player is active
     const buttonActive = isPlaying ? 'invert-0' : 'invert';
-
-    // extract media type from playerState
-    const context = playerState.context;
-
-    // determine if media type is playlist
-    const playlistStyles = context === 'playlist' ? 'invert-0' : 'invert';
 
     //console.log('Player:', player);
     console.log('Player state:', playerState);
@@ -86,15 +91,15 @@ export default function Player() {
 
                     <div className="flex px-32 justify-between bg-transparent">
 
-                        <button onClick={handlePrevious} className={`playerBtnSm ${playlistStyles}`}>
+                        <button onClick={handlePrevious} className={`playerBtnSm ${isPlayingPlaylist ? 'invert-0' : 'invert'}`}>
                             <img src="backward-step-solid.svg" alt="back" className="btnIconSm"></img>
                         </button>
 
                         <button onClick={handleTogglePlay} className={`playerBtn ${buttonActive}`}>
-                                <img src="/pause-solid.svg" alt="pause" className="btnIcon"></img>
+                            <img src="/pause-solid.svg" alt="pause" className="btnIcon"></img>
                         </button>
 
-                        <button onClick={handleSkip} className={`playerBtnSm ${playlistStyles}`}>
+                        <button onClick={handleSkip} className={`playerBtnSm ${isPlayingPlaylist ? 'invert-0' : 'invert'}`}>
                             <img src="forward-step-solid.svg" alt="skip" className="btnIconSm"></img>
                         </button>
                     </div>
