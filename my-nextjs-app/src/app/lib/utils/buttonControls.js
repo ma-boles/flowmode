ae55a53d10df2b578bc8a0793502a732774d0d17
ma-boles <ma_boles@hotmail.com>
2024-07-17 17:40:45 -0700

    git commit -m "updated onSelectPreview"

--- my-nextjs-app/src/app/lib/utils/buttonControls.js
+++ my-nextjs-app/src/app/lib/utils/buttonControls.js
@@ -1,6 +1,11 @@
-export const onSelectPreview = async(playlist, handleSetPreview, setAddedType, playSong, accessToken ) => {
+/*export const onSelectPreview = async(playlist, handleSetPreview, setAddedType, playSong, accessToken ) => {
     handleSetPreview(playlist.id, playlist.name);
     console.log('Preview util:', playlist.name);
     setAddedType('preview');
     await playSong(playlist.uri, accessToken);
+};*/
+
+export const onSelectPreview = (id, name) => {
+    handleSetPreview(id, name);
+    console.log('On select preview:', id, name);
 };
