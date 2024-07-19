87dd30f096bd7c35a9d0ed24f15d37b303f85614
ma-boles <ma_boles@hotmail.com>
2024-07-19 16:47:04 -0700

    git commit -m "made adjustments to togglePlay"

--- my-nextjs-app/src/app/lib/playerApi.js
+++ my-nextjs-app/src/app/lib/playerApi.js
@@ -27,22 +27,26 @@ const stopPlayback = (accessToken) => {
 
 const togglePlay = async (accessToken, playerState) => {
     try {
-        const { paused } = playerState || {};
+        if(!playerState) {
+            console.error('Player state is undefined.');
+            return;
+        }
 
-        if(paused === undefined) {
+        const { paused } = playerState;
+
+        if(typeof paused === 'undefined') {
             console.error('playerState does not have the paused property');
             return;
         }
         if(paused) {
-            await fetch('https://api.spotify.com/v1/me/player/pause', {
+            await fetch('https://api.spotify.com/v1/me/player/resume', {
                 method:'PUT',
                 headers: {
                     'Authorization': `Bearer ${accessToken}`
                 }
             });
         } else {
-            // if currently paused, resume the playback
-            await fetch('https://api.spotify.com/v1/me/player/play', {
+            await fetch('https://api.spotify.com/v1/me/player/pause', {
                 method: 'PUT',
                 headers : {
                     'Content-Type': 'application/json',
@@ -52,65 +56,6 @@ const togglePlay = async (accessToken, playerState) => {
         }
     } catch (error) {
         console.error('Error toggling play:', error);
-    }
-};
-
-const playSong = async (uri, accessToken) => {
-    await fetch(`https://api.spotify.com/v1/me/player/play`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        },
-        body: JSON.stringify({
-            uris: [uri],
-        }),
-    });
-};
-
-const playMedia = async (type, id, accessToken) => {
-    let url = `https://api.spotify.com/v1/me/player/play`;
-    let body;
-
-    switch(type) {
-        case 'track':
-            body = JSON.stringify({ uris: [`spotify:track:${id}`] });
-            break;
-        case 'album':
-            body = JSON.stringify({ context_uri: `spotify:album:${id}`});
-            break;
-        case 'playlist':
-            body = JSON.stringify({ context_uri: `spotify:playlist:${id}` });
-            break;
-        case 'podcast':
-            body = JSON.stringify({ context_uri: `spotify:episode:${id}` });
-            break;
-        case 'audiobook':
-            body = JSON.stringify({ context_uri: `spotify:audiobook:${id}` });
-            break;
-        default:
-            console.error('Unsupported media type');
-            return;
-    }
-
-    try {
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`
-            },
-            body: body,
-        });
-
-        if(response.ok) {
-            console.log(`${type.charAt(0).toUpperCase() + type.slice(1)} is playing`);
-        } else {
-            const errorData = await response.json();
-            console.error('Failes to play media:', response.status, response,statesText, errorData);
-        }
-    } catch (error) {
-        console.error('Error playing media:', error);
     }
 };
 
@@ -151,4 +96,76 @@ const previousTrack = async (accessToken) => {
     });
 };
 
-export { playTracks, stopPlayback, playSong, pausePlayback, resumePlayback, skipTrack, previousTrack, togglePlay, playMedia }
+
+// Triggering playback in preview mode
+const playSong = async (uri, accessToken) => {
+    await fetch(`https://api.spotify.com/v1/me/player/play`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            uris: [uri],
+        }),
+    });
+};
+
+const playAlbum = async (albumUri, accessToken) => {
+    await fetch(`https://api.spotify.com/v1/me/player/play`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            context_uri: albumUri,
+        }),
+    });
+};
+
+const playPlaylist = async (playlistUri, accessToken) => {
+    await fetch(`https://api.spotify.com/v1/me/player/play`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            context_uri: playlistUri,
+        }),
+    });
+};
+
+const playAudiobook = async (audiobookUri, accessToken) => {
+    await fetch(`https://api.spotify.com/v1/me/player/play`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            context_uri: audiobookUri,
+        }),
+    });
+};
+
+const playEpisode = async (episodeUri, accessToken) => {
+    await fetch(`https://api.spotify.com/v1/me/player/play`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            uris: [episodeUri],
+        }),
+    });
+};
+
+
+
+
+
+
+export { playTracks, stopPlayback, playSong, playAudiobook, playAlbum, playEpisode, playPlaylist, pausePlayback, resumePlayback, skipTrack, previousTrack, togglePlay }
