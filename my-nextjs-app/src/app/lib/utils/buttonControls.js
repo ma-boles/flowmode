212d50bdc35dc0574944748fd7a176e4af94a586
ma-boles <ma_boles@hotmail.com>
2024-07-18 17:56:13 -0700

    git commit -m "commented out all code"

--- my-nextjs-app/src/app/lib/utils/buttonControls.js
+++ my-nextjs-app/src/app/lib/utils/buttonControls.js
@@ -4,8 +4,21 @@
     setAddedType('preview');
     await playSong(playlist.uri, accessToken);
 };*/
+/**import { usePlaylistContext } from "@/app/contexts/PlaylistContext";
+
+const { handleSetFlowPlaylist, handleSetRestPlaylist, handleSetPreview } = usePlaylistContext();
 
 export const onSelectPreview = (id, name) => {
     handleSetPreview(id, name);
     console.log('On select preview:', id, name);
 };
+
+export const onSelectFlow = (id, name) => {
+    handleSetFlowPlaylist(id, name);
+    console.log('Select flow:', id, name);
+};
+
+export const onSelectRest = (id, name) => {
+    handleSetRestPlaylist(id, name);
+    console.log('Select rest:', id, name);
+};*/
