999825d92cdaf5988b0dd7d155799099b84c1db7
ma-boles <ma_boles@hotmail.com>
2024-07-20 15:37:07 -0700

    git commit -m "added breakpoints"

--- my-nextjs-app/tailwind.config.js
+++ my-nextjs-app/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    
+      screens: {
+        '2xs': '320px',
+        'xs': '375px',
+        'sm': '425',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1440px',
+        '2xl': '2560px'
+      }
     },
   plugins: [],
 }
