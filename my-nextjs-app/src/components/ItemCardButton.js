608bcbe0aba2f52425cd2ec84e5ef4d1c4f32866
ma-boles <ma_boles@hotmail.com>
2024-08-16 16:31:35 -0700

    git commit -m "removed commented out code"

--- my-nextjs-app/src/components/ItemCardButton.js
+++ my-nextjs-app/src/components/ItemCardButton.js
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import { playSong } from "@/app/lib/playerApi";
 import { useSession } from "next-auth/react";
 import usePlayer from "@/app/hooks/usePlayer";
+import { PlaylistContext } from "@/app/contexts/PlaylistContext";
 
-export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, onSelectPreview, flowPlaylistId, restPlaylistId, previewId }) {
+export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, onSelectPreview, flowPlaylistId, restPlaylistId, previewId, flowPlaylistName, restPlaylistName }) {
     const { data: session } = useSession();
     const accessToken = session.accessToken;
+
     const [addedType, setAddedType] = useState(null);
+
+    /*const handleFlowClick = () => {
+        if (isFlowAdded) {
+          // Unselect flow playlist if it's already selected
+          onSelectFlow(null, null);
+        } else {
+          onSelectFlow(playlist.id, playlist.name);
+        }
+      };*/
 
     const handleFlowClick = () => {
         if (playlist.id === flowPlaylistId) {
             setAddedType(null);
         } else {
-            console.log('Selecting the playlist');
-
             onSelectFlow(playlist.id, playlist.name);
             setAddedType('flow');
         }
@@ -30,8 +39,8 @@ export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, 
         // add code to unselect on second click
     };
 
-    const isFlowAdded = playlist.id === flowPlaylistId;
-    const isRestAdded = playlist.id === restPlaylistId;
+    const isFlowAdded = playlist.id === flowPlaylistId && playlist.name === flowPlaylistName;
+    const isRestAdded = playlist.id === restPlaylistId && playlist.name === restPlaylistName;
     const isPreviewAdded = playlist.id === previewId;
 
     const getButtonClass = (isAdded) => {
@@ -43,16 +52,14 @@ export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, 
                 <button className={`text-2xl font-bold ellipsis--grid--button`}>&#8230;</button>
                 <div className="ellipsis--grid--content text-center">
                     <ul>
-                        <li className={`py-1 font-semibold border-b border-solid border-gray-500 ${getButtonClass(isFlowAdded || addedType === 'flow')} `}
+                        <li className={`py-1 font-semibold border-b border-solid border-gray-500 ${getButtonClass(isFlowAdded || addedType === 'flow' )} `}
                             onClick={isFlowAdded || addedType === 'flow' ? null : handleFlowClick}>
-                            {/*onClick={isFlowAdded || addedType === 'flow' ? handleFlowClick : handleFlowClick}>*/}
-                            {isFlowAdded || addedType === 'flow' ? 'Added' : 'Flow'} {isFlowAdded || addedType === 'flow' && <span className="checkmark"></span>}
+                            {isFlowAdded || addedType === 'flow' ? 'Added' : 'Flow'} {/*isFlowAdded || addedType === 'flow' && <span className="checkmark"></span>*/}
                         </li>
 
                         <li className={`py-1 font-semibold border-b border-solid border-gray-500 ${getButtonClass(isRestAdded || addedType === 'rest')} `}
                             onClick={isRestAdded || addedType === 'rest' ? null : handleRestClick}>
-                            {/*onClick={isRestAdded || addedType === 'rest' ? handleRestClick : handleRestClick}>*/}
-                            {isRestAdded || addedType === 'rest' ? 'Added' : 'Rest'} {isRestAdded || addedType === 'rest' && <span className="checkmark"></span>}
+                            {isRestAdded || addedType === 'rest' ? 'Added' : 'Rest'} {/*isRestAdded || addedType === 'rest' && <span className="checkmark"></span>*/}
                         </li>
                     </ul>
                 </div>
