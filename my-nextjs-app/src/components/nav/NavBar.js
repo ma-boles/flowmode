6a560917c01e093a4db9ab5f7fe92c567f5a865a
ma-boles <ma_boles@hotmail.com>
2024-08-31 16:27:06 -0700

    git commit -m "removed unsued code"

--- my-nextjs-app/src/components/nav/NavBar.js
+++ my-nextjs-app/src/components/nav/NavBar.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import Modal from "../Modal";
+import DropdownMenu from "./DropdownMenu";
 
 
 const NavBar = () => {
     const { data: session } = useSession();
     const [isClient, setIsClient] = useState(false);
     const [currentPath, setCurrentPath] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         setIsClient(true);
@@ -53,9 +52,7 @@ const NavBar = () => {
                 </button>
             </div>
 
-
             <div className="dropdown">
-
                 {/* dropdown nav icon */}
                 <button className="dropdown-btn">
                     <div className="bar1"></div>
@@ -63,159 +60,8 @@ const NavBar = () => {
                     <div className="bar3"></div>
                 </button>
 
-                <div className="border border-solid rounded-sm dropdown-content">
-                   {/* <Link href='/get-started'>
-                        <p className="hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">Get Started</p>
-                    </Link>*/}
-                    <button className="flex w-full text-left border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link" onClick={() => setIsOpen(true)}>
-                        <div className="transparent">
-                            <img src="caret-left-solid.svg" className="m-auto mr-2 w-5 h-5 invert"></img>
-                        </div>
-                        Account
-                    </button>
-                    {currentPath === '/search' && (
-                        <>
-                        <Link href='/profile'>
-                            <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                                <div className="transparent">
-                                    <img src="user-regular.svg" className="m-auto mr-4 w-4 h-4 invert"></img>
-                                </div>
-                                Profile
-                            </div>
-                        </Link>
-                    </>
-                        )}
-
-                    {currentPath === '/profile' && (
-                        <>
-                        <Link href='/search'>
-                            <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                                <div className="transparent">
-                                    <img src="magnifying-glass-solid.svg" className="m-auto mr-4 w-4 h-4 invert"></img>
-                                </div>
-                                Search
-                            </div>
-                        </Link>
-                        </>
-                    )}
-
-                    {currentPath === '/about' && (
-                        <>
-                            <Link href="/profile">
-                                <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                                    <div className="transparent">
-                                        <img src="user-regular.svg" className="m-auto mr-4 w-4 h-4 invert"></img>
-                                    </div>
-                                    Profile
-                                </div>
-                            </Link>
-                            <Link href='/search'>
-                                <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                                    <div className="transparent">
-                                        <img src="magnifying-glass-solid.svg" className="m-auto mr-4 w-4 h-4 invert"></img>
-                                    </div>
-                                    Search
-                                </div>
-                            </Link>
-                        </>
-                    )}
-
-                        <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out aboutBtn">
-                            <div className="transparent">
-                                <img src="caret-left-solid.svg" className="m-auto mr-4 w-5 h-5 invert"></img>
-                            </div>
-                            About
-                            <div /* mini menu about/start */ className="aboutMenu">
-                                <Link href="/get-started">
-                                    <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                                        Get Started
-                                    </div>
-                                </Link>
-                                <Link href="/about">
-                                    <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                                        Mission・Story
-                                    </div>
-                                </Link>
-                            </div>
-                        </div>
-                    
-                    <button onClick={() => signOut('spotify')} className="flex w-full text-left hover:bg-red-600 transition duration-300 ease-in-out dropdown-link">
-                        <div className="transparent">
-                            <img src="right-from-bracket-solid.svg" className="m-auto mr-2 w-6 h-6 invert"></img>
-                        </div>
-                        Log Out
-                    </button>
-                </div>
+                <DropdownMenu />
             </div>
-
-            {/* mini about menus conditionally rendered */}
-            {currentPath ===  '/profile' && (
-                <div /* mini menu about/start */ className="aboutMenu">
-                    <Link href="/get-started">
-                        <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                            Get Started
-                        </div>
-                    </Link>
-                    <Link href="/about">
-                        <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                            Mission・Story
-                        </div>
-                    </Link>
-                </div>
-            )}
-            {currentPath === '/search' && (
-                <div /* mini menu about/start */ className="aboutMenu">
-                    <Link href="/get-started">
-                        <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                            Get Started
-                        </div>
-                    </Link>
-                    <Link href="/about">
-                        <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                            Mission・Story
-                        </div>
-                    </Link>
-                </div>
-            )}
-
-            {currentPath === '/about' && (
-                <div /* mini menu about/start */ className="aboutMenu2">
-                    <Link href="/get-started">
-                        <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                            Get Started
-                        </div>
-                    </Link>
-                    <Link href="/about">
-                        <div className="flex border-b border-solid border-gray-600 hover:bg-blue-600 transition duration-300 ease-in-out dropdown-link">
-                            Mission・Story
-                        </div>
-                    </Link>
-                </div>
-            )}
-
-
-            {/* Opt In modal */}
-            <div className="centered">
-                {isOpen && <Modal setIsOpen={setIsOpen} />}
-            </div>
-
-
-            {/* navigation buttons */}
-            {currentPath === '/get-started' && (
-                <div className="text-center rounded-md dropdown-nav">
-                    <button className="border border-solid border-green-600 font-semibold dropdown-nav-btn">Nav</button>
-                    <div className="border border-solid border-green-600 dropdown-nav-content">
-                        <Link href='/search'>
-                            <p className="p-2 border-b border-solid border-gray-400 font-semibold  hover:bg-green-600 transition duration-300 ease-in-out">Search</p>
-                        </Link>
-                        <a href="/profile">
-                            <p className="p-2 border-t border-solid border-gray-400 font-semibold hover:bg-green-600 transition duration-300 ease-in-out">Profile</p>
-                        </a>
-                    </div>
-                   
-                </div>
-                
-            )}
         </div>
     );
 };
