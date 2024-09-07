086d67d562bb27e48cc5f56f655955d1c5a87ff3
ma-boles <ma_boles@hotmail.com>
2024-09-07 16:24:51 -0700

    git commit -m "updated button styles"

--- my-nextjs-app/src/app/profile/page.js
+++ my-nextjs-app/src/app/profile/page.js
@@ -1,14 +1,10 @@
 'use client'
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import NavBar from "@/components/nav/NavBar";
 import Display from "@/components/playlists/Display";
-import Link from "next/link";
 import Player from "@/components/player/Player";
 import { useSession } from "next-auth/react";
 import { PlaylistProvider, usePlaylistContext } from "../contexts/PlaylistContext";
-import LastPlayed from "@/components/userdata/LastPlayed";
-import TotalMinutesFlow from "@/components/userdata/TotalMinutesFlow";
-import TotalMinutesRest from "@/components/userdata/TotalMinutesRest";
 import FlowCard from "@/components/userdata/FlowCard";
 import RestCard from "@/components/userdata/RestCard";
 
@@ -78,22 +74,18 @@ export default function Dashboard() {
                     <div /* top section */ className="mb-16 mt-8 flex justify-center">
 
                         <div /* minute totals div */>
-                            <div /* buttons div */ className="flex">
-                                <button className={`px-16 py-1 mb-2 border-r border-solid border-white  hover:bg-blue-600 ${showCard === 'flow' ? 'bg-blue-600' : 'bg-transparent'}`} onClick={handleFlowCard}>Flow</button>
-                                <button className={`px-16 mb-2 border-l border-solid border-white hover:bg-blue-600 ${showCard === 'rest' ? 'bg-blue-600' : 'bg-transparent'}`} onClick={handleRestCard}>Rest</button>
+                            <div /* buttons div */ className="flex justify-center">
+                                <button className={`px-16 py-1 mb-2 w-1/2 border-r border-solid border-white  hover:bg-blue-600 ${showCard === 'flow' ? 'bg-blue-600' : 'bg-transparent'}`} onClick={handleFlowCard}>Flow</button>
+                                <button className={`px-16 mb-2 w-1/2 border-l border-solid border-white hover:bg-blue-600 ${showCard === 'rest' ? 'bg-blue-600' : 'bg-transparent'}`} onClick={handleRestCard}>Rest</button>
                             </div>
 
                             {showCard === 'flow' &&
                                 <FlowCard />
-                                //<TotalMinutesFlow />
                             }
                             {showCard === 'rest' &&
                                 <RestCard />
-                                //<TotalMinutesRest />
                             }
                         </div>
-
-                        {/*<LastPlayed />*/}
 
                         <div className="mx-2 flex flex-col justify-center items-center">
                             <button className="mx-4 mb-4 h-32 w-80 font-semibold bg-blue-700 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out border-2 border-transparent focus:border-white" onClick={displayUserOwnedPlaylists}>My<br/>Playlists</button>
