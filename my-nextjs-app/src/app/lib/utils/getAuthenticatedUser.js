005fa840215b529aac7074e6113a899428083286
ma-boles <ma_boles@hotmail.com>
2024-09-19 14:32:20 -0700

    git commit -m "checks user and connects to database"

---
+++ my-nextjs-app/src/app/lib/utils/getAuthenticatedUser.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import dbConnect from "./dbConnect";
+import User from "../models/User";
+
+
+export async function getAuthenticatedUser (req) {
+    // Retrieve the JWT from the request
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    if (!token) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    // Extract from token
+    const { spotifyId, email } = token;
+
+    await dbConnect(); // Connect to database
+
+    // Query the database to find the user based on spotifyId or email
+    const user = await User.findOne({
+        $or: [{ spotifyId }, { email }]
+    });
+
+    // If user is not found, return an error
+    if(!user) {
+        return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    // Return the user object if found
+    return { user };
+}
