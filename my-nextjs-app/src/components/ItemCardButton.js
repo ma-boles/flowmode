f41c34bd8ec64af809d7204b2daf4b5b9704382a
ma-boles <ma_boles@hotmail.com>
2024-09-23 17:39:33 -0700

    git commit -m "added a favorites button"

--- my-nextjs-app/src/components/ItemCardButton.js
+++ my-nextjs-app/src/components/ItemCardButton.js
@@ -30,6 +30,10 @@ export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, 
         // add code to unselect on second click
     };
 
+    const handleFavorite = () => {
+        alert('Heart!');
+    };
+
     const isFlowAdded = playlist.id === flowPlaylistId && playlist.name === flowPlaylistName;
     const isRestAdded = playlist.id === restPlaylistId && playlist.name === restPlaylistName;
     const isPreviewAdded = playlist.id === previewId;
@@ -39,14 +43,18 @@ export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, 
             <div className="ellipsis--grid--div">
                 <button className={`text-2xl font-bold ellipsis--grid--button`}>&#8230;</button>
                 <div className="flex flex-col ellipsis--grid--content">
-                        <button className={`py-1 font-semibold border-b border-solid border-gray-500 active:bg-green-500 focus:bg-green-500 transition duration-150 ease-in-out `}
+                        <button className={`py-1 font-semibold border-b border-solid border-gray-500 active:bg-green-500 focus:bg-green-500 hover:bg-gray-500`}
                             onClick={isFlowAdded || addedType === 'flow' ? null : handleFlowClick}>
                             Flow
                         </button>
 
-                        <button className={`py-1 font-semibold border-b border-solid border-gray-500  active:bg-blue-600 focus:bg-blue-600 transition duration-150 ease-in-out `}
+                        <button className={`py-1 font-semibold border-b border-solid border-gray-500  active:bg-blue-600 focus:bg-blue-600 hover:bg-gray-500`}
                             onClick={isRestAdded || addedType === 'rest' ? null : handleRestClick}>
                             Rest
+                        </button>
+
+                        <button className={`py-1 font-semibold active:bg-red-500 focus:bg-red-500 hover:bg-gray-500`} onClick={handleFavorite}>
+                            <img src="heart-regular (1).svg" className="m-auto mb-1 w-5 h-5 invert"></img>
                         </button>
                 </div>
             </div>
