57fdcbbd089e03173766ed3fa3f6a789eed36f83
ma-boles <ma_boles@hotmail.com>
2024-09-24 17:08:24 -0700

    git commit -m "added favorites button click function"

--- my-nextjs-app/src/components/ItemCardButton.js
+++ my-nextjs-app/src/components/ItemCardButton.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { playSong } from "@/app/lib/playerApi";
 import { useSession } from "next-auth/react";
 import usePlayer from "@/app/hooks/usePlayer";
-import { PlaylistContext } from "@/app/contexts/PlaylistContext";
+import { PlaylistContext, usePlaylistContext } from "@/app/contexts/PlaylistContext";
 
 export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, onSelectPreview, flowPlaylistId, restPlaylistId, previewId, flowPlaylistName, restPlaylistName }) {
     const { data: session } = useSession();
     const accessToken = session.accessToken;
-
+    const { handleFavorite } = usePlaylistContext();
     const [addedType, setAddedType] = useState(null);
 
     const handleFlowClick = () => {
@@ -30,9 +30,46 @@ export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, 
         // add code to unselect on second click
     };
 
-    const handleFavorite = () => {
-        alert('Heart!');
+
+     //Function to update MongoDB with favorites title
+     async function handleFavoritesClick(favoritesTitle) {
+
+        // Validate favorites title before sending request
+        if(!favoritesTitle) {
+            console.error('Favorites title is missing.');
+            return;
+        }
+
+        console.log('Sending favorites title from button click:', favoritesTitle);
+        try {
+            const response = await fetch('/api/add-favorite', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    favoritesTitle,
+                }),
+            });
+
+            if(!response.ok) {
+                throw new Error('Failed to update recently played info');
+            }
+
+            const result = await response.json();
+            console.log('Update successful:', result);
+        } catch(error) {
+            console.error('Error adding favorites title', error);
+        }
     };
+
+    // Set favorite title when the button is clicked 
+    const handleAddToFavorites = () => {
+        console.log( 'Adding to favorites:', playlist.id, playlist.name);
+        //handleFavorite(playlist.id, playlist.name);
+        handleFavoritesClick(playlist.name);
+    };
+
 
     const isFlowAdded = playlist.id === flowPlaylistId && playlist.name === flowPlaylistName;
     const isRestAdded = playlist.id === restPlaylistId && playlist.name === restPlaylistName;
@@ -53,7 +90,7 @@ export default function ItemCardButton ({ playlist, onSelectFlow, onSelectRest, 
                             Rest
                         </button>
 
-                        <button className={`py-1 font-semibold active:bg-red-500 focus:bg-red-500 hover:bg-gray-500`} onClick={handleFavorite}>
+                        <button className={`py-1 font-semibold active:bg-red-500 focus:bg-red-500 hover:bg-gray-500`} onClick={handleAddToFavorites}>
                             <img src="heart-regular (1).svg" className="m-auto mb-1 w-5 h-5 invert"></img>
                         </button>
                 </div>
