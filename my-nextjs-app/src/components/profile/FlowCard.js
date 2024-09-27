72101cb4974d7999baf6d2f0305131cdf3738104
ma-boles <ma_boles@hotmail.com>
2024-09-26 17:10:02 -0700

    git commit -m "updated styling"

--- my-nextjs-app/src/components/profile/FlowCard.js
+++ my-nextjs-app/src/components/profile/FlowCard.js
@@ -7,11 +7,11 @@ export default function FlowCard({ data, favorites }) {
     return(
         <div className="flex">
             <div className="flex px-4 py-6 flow-card">
-                <div /* times div */>
+               {/* <div /* times div *//*>
                     <TotalMinutesFlow />
-                </div>
-                <div className="mx-4">
-                    <h2 className="px-10 my-4 font-bold text-2xl opacity-90">Recently Played:</h2>
+                </div>*/}
+                <div className="w-80">
+                    <h2 className="my-2 text-center font-bold text-xl opacity-90">Recently Played:</h2>
                     {data.length > 0 ? (
                         data.map((item, index) => (
                         <div key={index} className="flex flex-col m-2 border border-white rounded-sm opacity-90 cursor-pointer hover:bg-blue-600">
