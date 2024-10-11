eca33386c7a80be9a8473b4271720f59b31b757a
ma-boles <ma_boles@hotmail.com>
2024-10-11 14:13:09 -0700

    git commit -m "removed unused code, added tailwind styles for container div"

--- my-nextjs-app/src/components/profile/RestCard.js
+++ my-nextjs-app/src/components/profile/RestCard.js
@@ -1,22 +1,16 @@
 import React from "react";
-import TotalMinutesRest from "./TotalMinutesRest";
 import Favorites from "./Favorites";
 
 export default function RestCard({ data, favoritesList }) {
 
     return(
         <div className="flex">
-            <div className="flex px-4 py-6 flow-card">
-                {/*<div>
-                    <TotalMinutesRest />
-                </div>*/}
+            <div className="flex px-4 py-6 bg-black bg-opacity-20 border-2 border-white border-opacity-10 rounded-sm">
                 <div className="w-80">
                         <h2 className="mb-2 text-center font-bold text-xl opacity-90">Recently Played:</h2>
                         {data.length > 0 ? (
                             data.map((item, index) => (
                             <div key={index} className="flex flex-col m-2 border border-white rounded-sm opacity-90 cursor-pointer hover:bg-blue-600">
-                                {/* Display relevant data here */}
-                                {/*<p>{item.flow.title}</p>  Assuming the object has a 'title' field */}
                                 <h2 className="m-2 text-xl font-semibold">{item.rest?.title || 'No recent titles'}</h2>
                             </div>
                         ))
