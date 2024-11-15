d5850c66b4593b50fe3a0424db2784658bec6859
ma-boles <ma_boles@hotmail.com>
2024-11-14 16:07:06 -0800

    git commit -m "moved template button to its own component"

---
+++ my-nextjs-app/src/components/player/TemplateButton.js
@@ -0,0 +1,42 @@
+import { usePlaylistContext } from "@/app/contexts/PlaylistContext";
+import React from "react";
+
+export default function TemplateButton () {
+
+    const { flowPlaylistName, restPlaylistName } = usePlaylistContext();
+    const saveTemplate = async () => {
+        //alert('Template saved!');
+        //console.log(`Saving titles: Flow: ${flowPlaylistName}, Rest: ${restPlaylistName}`)
+
+        // Send title to database via api route
+        try {
+            console.log(`Saving titles: Flow: ${flowPlaylistName}, Rest: ${restPlaylistName}`);
+
+            // Call the API to save template
+            const response = await fetch('/api/add-template', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    flowPlaylistName,
+                    restPlaylistName
+                })
+            });
+
+            if(!response.ok) {
+                throw new Error('Failed to save template');
+            }
+            alert('Template saved!');
+            console.log('Template saved successfully!');
+        } catch (error) {
+            console.error('Error saving template', error);
+            alert('Failed to save template');
+        }
+    };
+    return (
+        <>
+            <button className="px-8 py-2 m-2 bg-purple-700"onClick={saveTemplate}>Save</button>
+        </>
+    )
+}
