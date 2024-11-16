34fa04db6d34b3516898d29314065d9f24d0580e
ma-boles <ma_boles@hotmail.com>
2024-11-15 16:15:36 -0800

    git commit -m "removed template form code, added  code to remove template"

--- my-nextjs-app/src/components/profile/Templates.js
+++ my-nextjs-app/src/components/profile/Templates.js
@@ -1,68 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import TemplateForm from "./TemplateForm";
 
 export default function Templates () {
+    const [templatesList, setTemplatesList] = useState([]);
 
     const sendToFlow = () => {
         alert('Sending template to flow player')
-        // send saved titles to flow player
+        // send saved titles (template, flow, rest) to flow player
     };
 
     const expandTitle = () => {
         alert('Expanding title')
         // expand section to show details
     };
+
+    const removeTemplate = async (title) => {
+        if (!title) {
+          console.log('No title provided');
+          return; // Prevent removing empty titles
+        }
+        try {
+          const response = await fetch('/api/remove-template', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ templatesTitle: title }),
+          });
+    
+          if (!response.ok){
+            throw new Error('Network response was not ok');
+          }
+          // Update the local state
+          const { templates } = await response.json();
+          setTemplatesList((prevTemplates) =>
+            prevTemplates.filter((item) => item.title !== title));
+
+          console.log(`Updated templates list: ${templates}`);
+
+        } catch (error) {
+          console.error('Error removing template:', error);
+        }
+      };
+
         return (
         <>
         <div className="p-4 bg-white bg-opacity-20 border border-white">
             <h1 className="mb-2 text-center font-bold text-xl opacity-90">Templates:</h1>
             <div className="my-2">
-                <div className="p-2 w-full bg-blue-700 border border-white text-start" >
-                    <ul className="flex font-bold justify-between">
-                        <div /* buttons div */ className="flex">
-                            <h2>Title </h2>
-                            <button className="px-1 font-light" onClick={expandTitle}>v</button>
-                        </div>
-                            <button className="px-1 bg-black hover:bg-white rounded-md" onClick={sendToFlow}>+</button>
-                    </ul>
-                    <li><span className="font-semibold">Flow:</span> Flow Title</li>
-                    <li><span className="font-semibold">Rest:</span> Rest Title</li>
+                <div className="flex w-full justify-between" >
+                    <div className="px-1 flex-grow bg-blue-700 w-4/5 border border-white text-start">
+                        <ul className="flex justify-between font-bold ">
+                            <div /* buttons div */ className="flex justify-between w-full">
+                                <h2>Title </h2>
+                                <button className="px-1 font-light bg-purple-700" onClick={expandTitle}>v</button>
+                            </div>
+                        </ul>
+                        <li><span className="font-semibold">Flow:</span> Flow Title</li>
+                        <li><span className="font-semibold">Rest:</span> Rest Title</li>
+                    </div>
+                    <div className="flex flex-col my-2 ml-2 justify-between">
+                        <button className="px-1 bg-black hover:bg-white rounded-md" onClick={sendToFlow}>+</button>
+                        <button className="px-2 bg-black hover:bg-white rounded-md" onClick={removeTemplate}>-</button>
+                    </div>
                 </div>
             </div>
-            <div className="p-2 border border-white bg-black bg-opacity-10 rounded-sm">
-                <form /* onSubmit={handleSubmit} */>
-                    <label htmlFor="title" className="font-semibold">Title: </label>
-                    <input 
-                        type="text"
-                        id="title"
-                        name="name"
-                        //value={formData.title}
-                        //onChange={handleChange}
-                        className="m-1 bg-transparent border-b border-white"
-                    />
-                    <br />
-                    <label htmlFor="title" className="font-semibold">Flow: </label>
-                    <input 
-                        type="text"
-                        id="flow"
-                        name="flow"
-                        //value={formData.title}
-                        //onChange={handleChange}
-                        className="bg-transparent border-b border-white"
-                    />
-                    <br />
-                    <label htmlFor="title" className="font-semibold">Rest: </label>
-                    <input 
-                        type="text"
-                        id="rest"
-                        name="rest"
-                        //value={formData.title}
-                        //onChange={handleChange}
-                        className="m-1 bg-transparent border-b border-white"
-                    />
-                    <br />
-                    <button className="mb-1 mt-2 w-full bg-blue-700 border border-white"/*type="submit*/>Save</button>
-                </form>
-            </div>
+
+           <TemplateForm />
+
         </div>
         </>
     )
