55e17104de7da410271be3c63ab898417538b2f4
ma-boles <ma_boles@hotmail.com>
2024-11-21 18:10:57 -0800

    git commit -m "updated styles"

--- my-nextjs-app/src/components/profile/Favorites.js
+++ my-nextjs-app/src/components/profile/Favorites.js
@@ -7,13 +7,12 @@ export default function Favorites() {
     //console.log(favoritesList);
    
            return (
-               <div className="w-80 text-center p-2 bg-black bg-opacity-20">
-                   <h2 className="my-2 text-xl font-bold">Favorites</h2>
+               <div className="w-80 h-85 p-1 text-center">
                    <div className="flex">
-                        <div className="w-full py-1 px-1">
+                        <div className="w-full pb-2 p-1">
                             {favoritesList && favoritesList.length > 0 ? (
                                 favoritesList.map((item, index) => (
-                                <div key={index} className="flex m-2 border border-white rounded-sm justify-between opacity-90 cursor-pointer">
+                                <div key={index} className="flex mb-1 border border-white rounded-sm justify-between opacity-90 cursor-pointer">
                                     <h2 className="m-2 text-lg font-semibold">{item.title || 'No titles added'}</h2>
                                     <button className="my-auto mr-2 w-6 h-6 bg-red-600 rounded-md hover:bg-green-600" onClick={() => removeFavorite(item.title)}>
                                         <img src="result.svg" className="m-auto w-6 h-6 invert"></img>
