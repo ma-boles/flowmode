8cf7702fc1f8af3b702c7b1d7601b4b7c5692d81
ma-boles <ma_boles@hotmail.com>
2024-12-02 23:02:52 -0800

    git commit -m "updated styles"

--- my-nextjs-app/src/components/profile/RestCard.js
+++ my-nextjs-app/src/components/profile/RestCard.js
@@ -6,7 +6,7 @@ export default function RestCard({ data, favoritesList, templatesList }) {
 
     return(
         <div className="flex h-40">
-            <div className="flex px-4 py-2 mx-1 border-2 border-blue-600 border-opacity-50 bg-white bg-opacity-5 rounded-lg">
+            <div className="flex px-4 py-2 mx-1 border-2 border-blue-600 border-opacity-50 bg-white bg-opacity-5 rounded-2xl">
                 <div className="w-80">
                         <h2 className="mb-2 text-center font-semibold text-xl opacity-90">Rest</h2>
                         {data.length > 0 ? (
