0db456af185b55566038a50520d06b55b3c9f60a
ma-boles <ma_boles@hotmail.com>
2024-12-26 17:50:24 -0800

    git commit -m "updated styles"

--- my-nextjs-app/src/components/profile/FlowCard.js
+++ my-nextjs-app/src/components/profile/FlowCard.js
@@ -4,8 +4,8 @@ import Favorites from "./Favorites";
 export default function FlowCard({ data, favoritesList }) {
 
     return(
-        <div className="flex h-40">
-            <div className="flex px-4 py-2 mb-2 mx-1 border-2 border-green-600 border-opacity-50 bg-white bg-opacity-5 rounded-2xl">
+        <div className="flex">
+            <div className="flex px-4 py-2 mb-2 mx-1 border-2 border-green-600 border-opacity-50 bg-black bg-opacity-20 rounded-2xl">
                 <div className="w-80">
                     <h2 className="mb-2 text-center font-semibold text-xl opacity-90">Flow</h2>
                     {data.length > 0 ? (
