25a7400d929c2385dea2b1974f5286532a5c711c
ma-boles <ma_boles@hotmail.com>
2025-01-10 16:04:31 -0800

    git commit -m "updated remove function"

--- my-nextjs-app/src/components/profile/Favorites.js
+++ my-nextjs-app/src/components/profile/Favorites.js
@@ -4,8 +4,12 @@ import { usePlaylistContext } from "@/app/contexts/PlaylistContext";
 export default function Favorites() {
     const { favoritesList, removeFavorite } = usePlaylistContext();
 
+    const handleRemoveFavorite = () => {
+        removeFavorite(item.favoritesId)
+    };
+
     //console.log(favoritesList);
-   
+
            return (
             <>
                <div className="p-2 mx-1 mt-2 text-center rounded-2xl bg-black bg-opacity-40 card-border">
@@ -16,7 +20,7 @@ export default function Favorites() {
                                 favoritesList.map((item, index) => (
                                 <div key={index} className="flex mb-1 border border-white rounded-sm justify-between opacity-90 cursor-pointer">
                                     <h2 className="m-2 text-lg font-semibold">{item.title || 'No titles added'}</h2>
-                                    <button className="my-auto mr-2 w-6 h-6 bg-red-600 rounded-md hover:bg-green-600" onClick={() => removeFavorite(item.title)}>
+                                    <button className="my-auto mr-2 w-6 h-6 bg-red-600 rounded-md hover:bg-green-600" onClick={handleRemoveFavorite}>
                                         <img src="result.svg" className="m-auto w-6 h-6 invert"></img>
                                     </button>
                                 </div>
