d6ab5eeffc6dba120677c22a3f5261abfa6d4dd3
ma-boles <ma_boles@hotmail.com>
2025-02-14 12:23:29 -0800

    git commit -m "updated layout css styles of links"

--- my-nextjs-app/src/components/nav/NavBar.js
+++ my-nextjs-app/src/components/nav/NavBar.js
@@ -39,45 +39,26 @@ const NavBar = () => {
 
     return(
         <>
-       {/*<div className={`${
-        isProfile ? 'fixed px-4 top-0 left-0 h-[537px] flex flex-col bg-black w-[225px] rounded-br-md'
-        :'flex px-8 justify-between w-full items-center navbar'
-        }`}
-        >*/}
-        <div className='fixed px-4 top-0 left-0 h-full flex flex-col bg-black w-[225px] rounded-br-md z-10'
-        >
-            {/*<div className="dropdown">*/}
-                {/*<button className={`${isProfile ? 'mb-2 mt-6' : 'p-0.5 my-2 bg-gray-400 profile-img'} cursor-pointer`}
-                 >*/}
-                <button className={`${isProfile ? 'mb-2 mt-6' : 'mb-2 mt-6'} cursor-pointer`}
-                 >
-                    {session.user.image ? (
-                        <a rel="noopener noreferrer" href="https://open.spotify.com/" target="_blank">
-                            <img src={session.user.image} alt="Spotify Profile" className="m-auto w-14 h-14 object-cover rounded-full"></img>
-                        </a>
-                    ) : (
-                        <a rel="noopener noreferrer" href="https://open.spotify.com/" target="_blank">
-                            <img src="user-regular.svg" alt="Spotify Profile" className="m-auto w-14 h-14 object-cover"></img>
-                        </a>
-                    )}
+        <div className='fixed px-4 top-0 left-0 h-full flex flex-col bg-black w-[225px] rounded-br-md z-10'>
+            <button /* spotify profile image */ className='mb-2 mt-6 cursor-pointer'>
+                {session.user.image ? (
+                    <a rel="noopener noreferrer" href="https://open.spotify.com/" target="_blank">
+                        <img src={session.user.image} alt="Spotify Profile" className="m-auto w-14 h-14 object-cover rounded-full"></img>
+                    </a>
+                ) : (
+                    <a rel="noopener noreferrer" href="https://open.spotify.com/" target="_blank">
+                        <img src="user-regular.svg" alt="Spotify Profile" className="m-auto w-14 h-14 object-cover"></img>
+                    </a>
+                )}
                 </button>
-                <span className="bg-white text-black tooltip">
-                    Open Spotify
-                </span>
+            <span className="bg-white text-black tooltip">
+                Open Spotify
+            </span>
 
-           {/* <div /* center logo *//* className={`${isProfile ? 'hidden': 'hidden'}`}>
-                <button className=" hover:bg-gray-600">
-                    <h1 className="p-1 text-5xl"><span className="font-bold text-green-500">f</span><span className="font-normal">m</span></h1>
-                </button>
-            </div>*/}
-
-            <div /*dropdown content */ className={`${isProfile ? 'flex flex-col' : 'flex flex-col'}`}>
+            <div /*sidenav buttons */ className={`${isProfile ? 'flex flex-col' : 'flex flex-col'}`}>
                 <ul className="flex flex-col border-t border-white-1">
-                    <button className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
-                        isProfile ? 'sideNav-link'
-                        :'sideNav-link'
-                        }`}
-                    /*className="flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out sideNav-link"*/ onClick={() => setIsOpen(true)}>
+                    <button className='flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out sideNav-link'
+                    onClick={() => setIsOpen(true)}>
                         Create Profile
                         <div className="transparent">
                             <img src="caret-left-solid.svg" className="m-auto ml-2 w-5 h-5 invert rotate-180"></img>
@@ -85,11 +66,7 @@ const NavBar = () => {
                     </button>
                     <Link href='/profile'>
                         {/*<div className="flex hover:bg-green-600 transition duration-300 ease-in-out dropdown-link">*/}
-                        <div className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
-                        isProfile ? 'sideNav-link'
-                        :'sideNav-link'
-                        }`}
-                        >
+                        <div className='flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out sideNav-link'>
                         Profile
                             <div className="transparent">
                                 <img src="user-regular.svg" className="m-auto ml-4 mt-1 w-4 h-4 invert"></img>
@@ -97,10 +74,7 @@ const NavBar = () => {
                         </div>
                     </Link>
                     <Link href='/search'>
-                        <div className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
-                        isProfile ? 'sideNav-link'
-                        :'sideNav-link'
-                        }`}
+                        <div className='flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out sideNav-link'
                         >
                         Search
                             <div className="transparent">
@@ -109,69 +83,28 @@ const NavBar = () => {
                         </div>
                     </Link>
                     <Link href="/get-started">
-                        <div className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
-                        isProfile ? 'sideNav-link'
-                        :'sideNav-link'
-                        }`}
-                        >
-                        Get Started
+                        <div className='flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out sideNav-link'>
+                        About
                             <div className="transparent">
                                 <img src="circle-info-solid (1).svg" className="m-auto ml-4 mt-1 w-4 h-4 invert"></img>
                             </div>
                         </div>
                     </Link>
-                    <Link href="/about">
-                        <div className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
-                        isProfile ? 'sideNav-link'
-                        :'sideNav-link'
-                        }`}
-                        >
-                        Mission・Story
-                            <div className="transparent">
-                                <img src="pen-to-square-regular (1).svg" className="m-auto ml-4 mt-1 w-4 h-4 invert"></img>
-                            </div>
-                        </div>
-                    </Link>
-                    {/*<div className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
-                        isProfile ? 'sideNav-link'
-                        :'dropdown-link'
-                        }`}
-                    >
-                        Settings
-                            <div className="transparent">
-                                <img src="pen-to-square-regular (1).svg" className="m-auto ml-4 mt-1 w-4 h-4 invert"></img>
-                            </div>
-                    </div>*/}
-                    <button onClick={() => signOut('spotify')} 
-                    className={`flex justify-between bg-red-600 w-full text-left hover:bg-gray-600 transition duration-300 ease-in-out ${
-                        isProfile ? 'sideNav-link'
-                        :'sideNav-link'
-                        }`}
-                        >
-                    Log Out
 
+                    <button onClick={() => signOut('spotify')}
+                    className='flex justify-between bg-red-600 w-full text-left hover:bg-gray-600 transition duration-300 ease-in-out sideNav-link'>
+                    Log Out
                         <div className="transparent">
                             <img src="right-from-bracket-solid.svg" className="m-auto ml-2 w-5 h-5 invert"></img>
                         </div>
                     </button>
-
                 </ul>
             </div>
+
             {/* Account modal */}
             <div className="centered">
                 {isOpen && <Account setIsOpen={setIsOpen} />}
             </div>
-
-            {/*<div className="dropdown">
-                {/* dropdown nav icon *//*}
-                <button className={`${ isProfile ? 'hidden' :
-                'dropdown-btn'}`}>
-                    <div className="bar1"></div>
-                    <div className="bar2"></div>
-                    <div className="bar3"></div>
-                </button>
-                <DropdownMenu />
-            </div>*/}
         </div>
 
         </>
@@ -182,3 +115,57 @@ const NavBar = () => {
 export default NavBar;
 
 {/*className="flex px-8 justify-between w-full items-center navbar"*/}
+
+{/* <div /* center logo *//* className={`${isProfile ? 'hidden': 'hidden'}`}>
+                <button className=" hover:bg-gray-600">
+                    <h1 className="p-1 text-5xl"><span className="font-bold text-green-500">f</span><span className="font-normal">m</span></h1>
+                </button>
+            </div>*/}
+
+
+{/*<Link href="/about">
+                        <div className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
+                        isProfile ? 'sideNav-link'
+                        :'sideNav-link'
+                        }`}
+                        >
+                        Mission・Story
+                            <div className="transparent">
+                                <img src="pen-to-square-regular (1).svg" className="m-auto ml-4 mt-1 w-4 h-4 invert"></img>
+                            </div>
+                        </div>
+                    </Link>*/}
+
+
+
+ {/*<div className={`flex w-full text-left justify-between hover:bg-green-600 transition duration-300 ease-in-out ${
+                        isProfile ? 'sideNav-link'
+                        :'dropdown-link'
+                        }`}
+                    >
+                        Settings
+                            <div className="transparent">
+                                <img src="pen-to-square-regular (1).svg" className="m-auto ml-4 mt-1 w-4 h-4 invert"></img>
+                            </div>
+                    </div>*/}
+
+ {/*<div className={`${
+        isProfile ? 'fixed px-4 top-0 left-0 h-[537px] flex flex-col bg-black w-[225px] rounded-br-md'
+        :'flex px-8 justify-between w-full items-center navbar'
+        }`}
+        >*/}
+
+{/*<div className="dropdown">
+                {/* dropdown nav icon *//*}
+                <button className={`${ isProfile ? 'hidden' :
+                'dropdown-btn'}`}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </button>
+                <DropdownMenu />
+            </div>*/}
+
+{/*<div className="dropdown">*/}
+                {/*<button className={`${isProfile ? 'mb-2 mt-6' : 'p-0.5 my-2 bg-gray-400 profile-img'} cursor-pointer`}
+                 >*/}
